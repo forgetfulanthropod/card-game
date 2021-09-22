8c5c64ef41973e47592986c668f06d47112bb994
Luke Harold Miles <luke@cs.uky.edu>
2021-09-22 12:45:25 -0400

    styled component for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react"
 import { Toaster } from "react-hot-toast"
 import Fighter from "./Fighter"
-import styles from './App.module.css'
+import styled from "styled-components"
+
+const AppDiv = styled.div`
+    width: 100vw;
+    height: 56.25vw;
+    max-height: 100vh;
+    max-width: 177.78vh;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border: 1px solid red;
+`
+
 export default function App(): JSX.Element {
-    return <div className={styles.app}>
+    return <AppDiv>
         <Toaster />
         <h2>Hello, world</h2>
         <Fighter />
-    </div>
+    </AppDiv>
 }
