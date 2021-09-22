0ddbd2d713423974d3f4e0dcf4c99d28b6a882b3
Luke Harold Miles <luke@cs.uky.edu>
2021-09-22 16:19:42 -0400

    add frog knights; shrink images; add shrink-png.sh

---
+++ scripts/shrink-png.sh
@@ -0,0 +1,4 @@
+set -x
+cp $1.png $1.png.bak
+sips --resampleWidth $2 $1.png --out $1-$2.png
+pngquant 64 --skip-if-larger --strip --ext=.png --force $1-$2.png
--- src/Fighter.tsx
+++ src/Fighter.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react"
 import styled, { keyframes, css } from "styled-components"
-import skeletonPng from "./assets/Skeleton_Warrior_sprite.png"
+import skeletonPng from "./assets/Skeleton_Warrior_sprite-200.png"
+import frogknightPng from "./assets/Frog_Knight_sprite-200.png"
 import startPng from "./assets/start.png"
 
 
-const positions = makePositions()
-
-function makePositions(): [number, number][] {
-    const hGap = 18
-    const vGap = 15
-    const [x0, y0] = [70, 35]
+const skeletonPositions = makePositions(70, 35, 18, 15)
+const frogknightPositions = makePositions(10, 35, 18, 15)
+function makePositions(x0: number, y0: number, hGap: number, vGap: number): [number, number][] {
     return [
         [x0, y0],
         [x0 + hGap, y0],
@@ -17,15 +15,17 @@ function makePositions(): [number, number][] {
         [x0 + hGap / 2, y0 + vGap],
         [x0, y0 + vGap * 2],
         [x0 + hGap, y0 + vGap * 2],
-    ]//.map(([x, y]) => [x + "%", y + "%"])
+    ]
 }
 
 export default function Fighter(): JSX.Element {
     // const [width, setWidth] = useState(200)
     return <div>
         <Start />
-        {positions.map(([x, y]) =>
-            <Skeleton key={x + y} x={x} y={y} />)}
+        {skeletonPositions.map(([x, y]) =>
+            <Character key={x + y} x={x} y={y} Thing={SkeletonImg} direction={1} />)}
+        {frogknightPositions.map(([x, y]) =>
+            <Character key={x + y} x={x} y={y} Thing={FrognightImg} direction={-1} />)}
         {/* <div>
             <button onClick={() => setShake(true)}>Punch</button>
             <button onClick={() => setWidth(400)}>Kick</button>
@@ -34,18 +34,25 @@ export default function Fighter(): JSX.Element {
     </div>
 }
 
-function Skeleton(props: { x: number, y: number }) {
-    const [shake, setShake] = useState(false)
+function Character(props: { x: number, y: number, Thing: typeof Sprite, direction: -1 | 1 }): JSX.Element {
+    const Thing = props.Thing
+    const [hasEffect, setHasEffect] = useState(false)
     useEffect(() => {
-        if (!shake) return () => { }
-        const t = setTimeout(() => setShake(false), 500)
+        if (!hasEffect) return () => { }
+        const t = setTimeout(() => setHasEffect(false), 500)
         return () => clearTimeout(t)
-    }, [shake])
+    }, [hasEffect])
+    return <div onClick={() => setHasEffect(true)}>
+        {hasEffect
+            ?
+            <>
+                <Thing shake={hasEffect} x={props.x + .5 * props.direction} y={props.y} blur={true} />
+                <Thing shake={hasEffect} x={props.x} y={props.y} color="red" />
+            </>
+            :
+            <Thing shake={hasEffect} x={props.x} y={props.y} />
+        }
 
-    return <div>
-        <SkeletonImg shake={shake} x={props.x + .3} y={props.y} color="red" />
-        <SkeletonImg shake={shake} x={props.x + .6} y={props.y} blur={true} />
-        <SkeletonImg shake={shake} x={props.x} y={props.y} onClick={() => setShake(true)} />
     </div>
 }
 
@@ -57,7 +64,8 @@ const Start = styled.img.attrs({ src: startPng })`
     top: 25%;
 `
 
-const SkeletonImg = styled.img.attrs({ src: skeletonPng, width: 200 })
+
+const Sprite = styled.img.attrs({ src: skeletonPng, width: 200 })
     <{ shake: boolean, x: number, y: number, color?: string, blur?: boolean }>`
     ${p => p.shake && css`animation: ${shake} 0.5s;`}
 
@@ -66,9 +74,14 @@ const SkeletonImg = styled.img.attrs({ src: skeletonPng, width: 200 })
     top: ${p => p.y}%;
     width: 200px;
     ${p => p.blur === true && `filter: blur(2px);`}
-    ${p => p.color != null && css`filter: opacity(0.5) drop-shadow(0 0 0 ${p.color});`}
+    ${p => p.color != null && css`
+        filter: opacity(0.5) drop-shadow(0 0 0 ${p.color});
+    `}
     /* box-shadow: 5px 6px 7px black; */
 `
+const FrognightImg = styled(Sprite).attrs({ src: frogknightPng, width: 200 })``
+const SkeletonImg = styled(Sprite).attrs({ src: skeletonPng })``
+
 
 const shake = keyframes`
     0% { transform: translate(1px, 1px) rotate(0deg); }
