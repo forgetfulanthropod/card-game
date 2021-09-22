866a6dc695e2db7c3212e916960772b923cb605d
Luke Harold Miles <luke@cs.uky.edu>
2021-09-22 12:40:24 -0400

    add global.css and make app fixed aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import { Toaster } from "react-hot-toast"
 import Fighter from "./Fighter"
-import './App.css'
-
+import styles from './App.module.css'
 export default function App(): JSX.Element {
-    return <div>
+    return <div className={styles.app}>
         <Toaster />
         <h2>Hello, world</h2>
         <Fighter />
