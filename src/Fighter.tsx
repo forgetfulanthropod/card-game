677a410d07955fdb07cb19b05375ec34870079fe
Luke Harold Miles <luke@cs.uky.edu>
2021-09-22 14:44:38 -0400

    use `keyframes` function for `shake`

--- src/Fighter.tsx
+++ src/Fighter.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import styled from "styled-components"
+import styled, { keyframes, css } from "styled-components"
 import skeletonPng from "./assets/Skeleton_Warrior_sprite.png"
 import startPng from "./assets/start.png"
 
@@ -30,19 +30,19 @@ const Start = styled.img.attrs({ src: startPng })`
 `
 
 const Skeleton = styled.img.attrs({ src: skeletonPng }) <{ shake: boolean }>`
-    ${p => p.shake && "animation: shake 0.5s;"}
+    ${p => p.shake && css`animation: ${shake} 0.5s;`}
+`
 
-    @keyframes shake {
-        0% { transform: translate(1px, 1px) rotate(0deg); }
-        10% { transform: translate(-1px, -2px) rotate(-1deg); }
-        20% { transform: translate(-3px, 0px) rotate(1deg); }
-        30% { transform: translate(3px, 2px) rotate(0deg); }
-        40% { transform: translate(1px, -1px) rotate(1deg); }
-        50% { transform: translate(-1px, 2px) rotate(-1deg); }
-        60% { transform: translate(-3px, 1px) rotate(0deg); }
-        70% { transform: translate(3px, 1px) rotate(-1deg); }
-        80% { transform: translate(-1px, -1px) rotate(1deg); }
-        90% { transform: translate(1px, 2px) rotate(0deg); }
-        100% { transform: translate(1px, -2px) rotate(-1deg); }
-    }
+const shake = keyframes`
+    0% { transform: translate(1px, 1px) rotate(0deg); }
+    10% { transform: translate(-1px, -2px) rotate(-1deg); }
+    20% { transform: translate(-3px, 0px) rotate(1deg); }
+    30% { transform: translate(3px, 2px) rotate(0deg); }
+    40% { transform: translate(1px, -1px) rotate(1deg); }
+    50% { transform: translate(-1px, 2px) rotate(-1deg); }
+    60% { transform: translate(-3px, 1px) rotate(0deg); }
+    70% { transform: translate(3px, 1px) rotate(-1deg); }
+    80% { transform: translate(-1px, -1px) rotate(1deg); }
+    90% { transform: translate(1px, 2px) rotate(0deg); }
+    100% { transform: translate(1px, -2px) rotate(-1deg); }
 `
