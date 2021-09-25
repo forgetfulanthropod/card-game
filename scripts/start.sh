bc5d9720d6d31797385e18c9d4191b7567dec40c
Luke Harold Miles <luke@cs.uky.edu>
2021-09-25 17:33:08 -0400

    use locally installed serve command

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@ if [ "$ESBUILD_DONT_SERVE" = "yes" ];
 then
     node scripts/esbuild.js watch
 else
-    bash scripts/run-parallel.sh "npx serve -s ./build -p 3000 --no-clipboard" "node scripts/esbuild.js watch"
+    bash scripts/run-parallel.sh "npm run serve" "node scripts/esbuild.js watch"
 fi
