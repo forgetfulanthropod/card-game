a632798892e2883e38800158cc7f683cf626cc49
Luke Harold Miles <luke@cs.uky.edu>
2021-09-25 17:42:25 -0400

    oops put stories back into src

--- src/features/battle/stories/HealthBar.stories.tsx
+++ src/features/battle/stories/HealthBar.stories.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 import { ComponentStory, Meta } from '@storybook/react'
 
-import HealthBar from './HealthBar'
+import HealthBar from '../components/HealthBar'
 
 export default {
     component: HealthBar,
@@ -32,5 +32,12 @@ Min.args = {
     value: 1,
     max: 100
 };
+
+export const Half = Template.bind({});
+Half.args = {
+    value: 50,
+    max: 100
+};
+
 // export const Mid: React.VFC<typeof HealthBar> = () => <HealthBar value={70} max={100} />
 // export const Low: React.VFC<typeof HealthBar> = () => <HealthBar value={20} max={100} />
