f389ce416d55872b7d19f254b433d09624081e28
Luke Harold Miles <luke@cs.uky.edu>
2021-09-30 15:06:52 -0400

    chest background overlay working

--- src/features/battle/components/Chest.tsx
+++ src/features/battle/components/Chest.tsx
@@ -11,6 +11,8 @@ export default function Chest(props: { size: SizeQ }): JSX.Element {
     const { ticker, screen } = usePixiApp()
     const [lid, setLid] = useState<PixiSprite | null>(null)
     const [body, setBody] = useState<PixiSprite | null>(null)
+    console.log('chest render')
+
 
     useEffect((): void | Callback => {
         if (lid == null) return undefined
@@ -25,23 +27,25 @@ export default function Chest(props: { size: SizeQ }): JSX.Element {
 
     const baseSize = 500
     const scale = props.size.width == null ? 1 : baseSize / props.size.width
-    return <Container scale={scale} position={{ x: screen.width / 2, y: screen.height / 2 }}>
-        {/* <Overlay /> */}
-        <Sprite
-            // anchor={0.5}
-            anchor={{ x: 1, y: .3 }}
-            ref={setBody}
-            image={chestBodyPng} />
-        <Sprite
-            ref={s => setLid(s)}
-            image={chestLidPng}
-            // pivot={{ x: 100, y: 1 }}
-            pivot={{ x: -50, y: 0 }}
-            // anchor={{ x: 0, y: 0 }}
-            anchor={{ x: .95, y: .4 }}
-        // position={1}
-        />
-        {/* <Graphics draw={g => {
+    return <>
+        {props.size.width != null && <NewOverlay size={props.size as Size} />}
+        <Container scale={scale} position={{ x: screen.width / 2, y: screen.height / 2 }}>
+
+            <Sprite
+                // anchor={0.5}
+                anchor={{ x: 1, y: .3 }}
+                ref={setBody}
+                image={chestBodyPng} />
+            <Sprite
+                ref={s => setLid(s)}
+                image={chestLidPng}
+                // pivot={{ x: 100, y: 1 }}
+                pivot={{ x: -50, y: 0 }}
+                // anchor={{ x: 0, y: 0 }}
+                anchor={{ x: .95, y: .4 }}
+            // position={1}
+            />
+            {/* <Graphics draw={g => {
             if (lid == null) return
             drawCircle(g, 'red', lid.pivot.x, lid.pivot.y, 40)
             drawCircle(g, 'green', lid.anchor.x, lid.anchor.y, 20)
@@ -49,16 +53,19 @@ export default function Chest(props: { size: SizeQ }): JSX.Element {
             drawCircle(g, 'orange', body.anchor.x, body.anchor.y, 10)
         }}
         /> */}
-    </Container>
+        </Container>
+    </>
 }
 
-const Overlay = PixiComponent<EmptyObject, PixiSprite>('PixiBackground', {
-    create: (_props) => {
+
+const NewOverlay = PixiComponent<{ size: Size }, PixiSprite>('NewOverlay', {
+    create: (props) => {
+        console.log('new overlay createad')
         const bg = new PixiSprite(PixiTexture.WHITE)
-        bg.width = 800
-        bg.height = 500
-        bg.tint = 0xFF0000
-        bg.alpha = 0.3
+        bg.width = props.size.width
+        bg.height = props.size.height
+        bg.tint = 0x000000
+        bg.alpha = 0.5
         return bg
     },
 })
