232ee54116e2e81b555e145ebeaf4751a0e582fa
Luke Harold Miles <luke@cs.uky.edu>
2021-10-04 04:07:22 -0400

    add width, height, and pivot to MySprite

--- src/newbattle/mypixi.ts
+++ src/newbattle/mypixi.ts
@@ -7,21 +7,36 @@ export function MySprite(args: {
     anchor?: number | Pair
     position?: Pair
     scale?: number | Pair
+    width?: number
+    height?: number
+    pivot?: number | Pair
 }): Sprite {
     const s = Sprite.from(args.src)
 
-    if (args.anchor)
-        if (Array.isArray(args.anchor))
+    if (args.anchor) {
+        if (Array.isArray(args.anchor)) {
             s.anchor.set(...args.anchor)
-        else
+        } else {
             s.anchor.set(args.anchor)
+        }
+    }
 
-    if (args.position) s.position.set(...args.position)
+    if (args.position) { s.position.set(...args.position) }
     if (args.scale) {
         if (Array.isArray(args.scale)) {
             s.scale.set(...args.scale)
         } else {
             s.scale.set(args.scale)
+        }
+    }
+
+    if (args.width) { s.width = args.width }
+    if (args.height) { s.height = args.height }
+    if (args.pivot) {
+        if (Array.isArray(args.pivot)) {
+            s.pivot.set(...args.pivot)
+        } else {
+            s.pivot.set(args.pivot)
         }
     }
     return s
