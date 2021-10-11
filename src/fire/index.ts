8baa69b8e46747be6c4fda7f186eb3a113cdeb62
Charlie C Miles <charliecmiles@gmail.com>
2021-10-11 14:23:10 -0400

    deletes credentials

--- src/fire/index.ts
+++ src/fire/index.ts
@@ -1,16 +1,13 @@
 import { getApp, initializeApp } from 'firebase/app'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 import { Functions, getFunctions, connectFunctionsEmulator, httpsCallable } from 'firebase/functions'
 
 console.log('initializing app but not storing result -- not sure if that works')
 initializeApp({
-    apiKey: 'AIzaSyDavWqGtoB5JavUmkz_l4EdtFhxETFkB2o',
-    authDomain: 'kaiju-75e84.firebaseapp.com',
     projectId: 'kaiju-75e84',
-    storageBucket: 'kaiju-75e84.appspot.com',
-    messagingSenderId: '1004107907735',
-    appId: '1:1004107907735:web:d180cfa470b5b5c6365fd2'
 })
 
+const db = getFirestore()
 type FunctionName =
     | 'makeUppercase'
     | 'helloWorld'
@@ -24,4 +21,8 @@ export function makeCall(): void {
     connectFunctionsEmulator(functions, 'localhost', 5001)
     const helloWorld = getFunction(functions, 'helloWorld')
     helloWorld().then(result => console.log('FIREBASE CLOUD FUNCTION:', result))
+
+
+    // firebaseApps previously initialized using initializeApp()
+    connectFirestoreEmulator(db, 'localhost', 8080)
 }
