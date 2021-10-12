73a941212bdbd606192a4a6a3083ce2b763eb583
Luke Harold Miles <luke@cs.uky.edu>
2021-10-12 09:34:17 -0400

    add object utility functions

--- client/util/index.ts
+++ client/util/index.ts
@@ -32,3 +32,24 @@ export function set<T>(arr: readonly T[], i: number, t: T): T[] {
     newArr[i] = t
     return newArr
 }
+
+export function length<K extends string | number>(obj: Record<K, unknown>): number {
+    return Object.keys(obj).length
+}
+
+export function valMap<K extends string | number, V, V2>(obj: Record<K, V>, f: (v: V) => V2): V2[] {
+    return (Object.values(obj) as V[]).map(v => f(v))
+
+}
+export function keyMap<K extends string | number, V, X>(obj: Record<K, V>, f: (k: string) => X): X[] {
+    return Object.keys(obj).map(f)
+}
+export function entryMap<K extends string | number, V, X>(obj: Record<K, V>, f: (k: string, v: V) => X): X[] {
+    return (Object.entries(obj) as [string, V][]).map(([k, v]) => f(k, v))
+}
+export function objMap<K extends string | number, V, X>(obj: Record<K, V>, f: (k: string, v: V) => X): Record<K, X> {
+    // @ts-ignore
+    const o: Record<K, X> = {};
+    (Object.entries(obj) as [K, V][]).forEach(([k, v]) => o[k] = f(k as string, v))
+    return o
+}
