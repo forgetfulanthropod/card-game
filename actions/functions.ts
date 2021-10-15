a647a8f661ab72fa39879b2cc65ecc038f20c8f9
Luke Harold Miles <luke@cs.uky.edu>
2021-10-15 03:26:06 -0400

    Add hello, getRulebook, startGame to /functions. And stub for doCharacterAction.

--- actions/functions.ts
+++ actions/functions.ts
@@ -1,4 +1,4 @@
-import type { ChangeScene, ChooseDoor, PutUpDoors } from '@shared/actions'
+import type { ChangeScene, ChooseDoor, DoCharacterAction, GetRulebook, Hello, PutUpDoors, StartGame } from '@shared/actions'
 import type { BattleScene } from '@shared/index'
 
 import { tree } from '.'
@@ -21,4 +21,17 @@ export const chooseDoor_: ChooseDoor = door => {
     const room = makeRoom({ door, dungeonName: 'cool dungeon', roomsPassed: 0 })
     scene.apply('allCharacters', ac => ({ ...objFilter(ac, (_, c) => c.isPc), ...room.enemies }))
     scene.set('doors', null)
+}
+
+export const hello: Hello = () => { return 'hello' }
+export const getRulebook: GetRulebook = () => { return rulebook }
+export const startGame: StartGame = async () => {
+    const scene = await tree.select('scene').get()
+    if (scene.name !== 'battle') {
+        throw Error('tried to start game when not in battle scene') // error should be caught by wrapper
+    }
+    return scene
+}
+export const doCharacterAction: DoCharacterAction = () => {
+    // TODO
 }
