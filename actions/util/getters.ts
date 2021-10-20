eac3d37507d67532a20667645de562bd52f7a043
charlie <charliecmiles@gmail.com>
2021-10-20 14:32:12 -0400

    Revert "Cache firestore.get in firecursor: works!"

--- actions/util/getters.ts
+++ actions/util/getters.ts
@@ -2,10 +2,9 @@ import type { BattleScene } from '@shared/battleTypes'
 import type { Gamestate } from '@shared/datamodel'
 import type { EntryScene } from '@shared/entryTypes'
 import { firestore } from 'firebase-admin'
-import { memoize } from 'lodash'
 
 import type { FireCursor } from './FireCursor'
-import { makeFireCursor } from './FireCursor'
+import { makeFBCursor } from './FireCursor'
 
 
 export async function getEntryScene(username: 'alice'): Promise<FireCursor<Gamestate, EntryScene>> {
@@ -25,10 +24,10 @@ export async function getBattleScene(username: 'alice'): Promise<BattleCursor> {
     return scene as BattleCursor
 }
 
-export const getGameStateCursor = memoize(async function getGameStateCursor(username: 'alice'): Promise<FireCursor<Gamestate>> {
+export async function getGameStateCursor(username: 'alice'): Promise<FireCursor<Gamestate>> {
     const docRef = firestore().collection('users').doc(username)
     if (!(await docRef.get()).exists) {
         throw Error('could not find user doc')
     }
-    return makeFireCursor(docRef, [])
-})
+    return makeFBCursor(docRef, [])
+}
