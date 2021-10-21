4f4a51d1a0633ff3071250250079ab5714d424be
Luke Harold Miles <luke@cs.uky.edu>
2021-10-21 00:06:05 -0400

    avoid unneeded flushes

--- actions/util/FireCursor.ts
+++ actions/util/FireCursor.ts
@@ -9,32 +9,53 @@ const config = {
     logDiffOnFlush: false
 }
 
+class CursorRoot<Root extends Objectish> {
+    constructor(
+        public docRef: firestore.DocumentReference<Root>,
+        public data: Root,
+        public changeMade: boolean,
+    ) { }
+}
+
 export class FireCursor<Root extends Objectish, Sub = Root> {
-    constructor(private docRef: firestore.DocumentReference<Root>,
+    constructor(public R: CursorRoot<Root>,
         private path: string[],
-        public rootData: Root) { }
+    ) { }
     select<K extends keyof Sub>(k: K): FireCursor<Root, Sub[K]> {
         const newPath = [...this.path, k as string]
-        return new FireCursor<Root, Sub[K]>(this.docRef, newPath, this.rootData)
+        return new FireCursor<Root, Sub[K]>(this.R, newPath)
     }
-    get(): Sub { return get(this.rootData, this.path) }
-    getK<K extends keyof Sub>(k: K): Sub[K] { return get(this.rootData, [...this.path, k]) }
-    set(value: Sub): void { set(this.rootData, this.path, value) }
+    get(): Sub { return get(this.R.data, this.path) }
+    getK<K extends keyof Sub>(k: K): Sub[K] { return get(this.R.data, [...this.path, k]) }
+    set(value: Sub): void {
+        this.R.changeMade = true
+        set(this.R.data, this.path, value)
+    }
     setK<K extends keyof Sub>(key: K, value: Sub[K]): void {
-        // console.log(`firecursor setting ${key} to ${JSON.stringify(value)}`)
-        // debugger
-        set(this.rootData, [...this.path, key], value)
+        this.R.changeMade = true
+        set(this.R.data, [...this.path, key], value)
     }
-    apply(func: (prev: Sub) => Sub): void { update(this.rootData, this.path, func) }
-    applyK<K extends keyof Sub>(key: K, func: (prev: Sub[K]) => Sub[K]): void { update(this.rootData, [...this.path, key], func) }
+    apply(func: (prev: Sub) => Sub): void {
+        this.R.changeMade = true
+        update(this.R.data, this.path, func)
+    }
+    applyK<K extends keyof Sub>(key: K, func: (prev: Sub[K]) => Sub[K]): void {
+        this.R.changeMade = true
+        update(this.R.data, [...this.path, key], func)
+    }
     async flush(): Promise<void> {
+        if (!this.R.changeMade) {
+            console.log('congratulations you skipped an unnecessary change')
+            return
+        }
         // TODO?: update with diff instead of full thing?
         if (config.logDiffOnFlush) {
-            const prev = (await this.docRef.get()).data()
-            const differences = diff(prev, this.rootData)
+            const prev = (await this.R.docRef.get()).data()
+            const differences = diff(prev, this.R.data)
             console.log('flushing data with differences...', JSON.stringify(differences))
         }
-        await this.docRef.set(this.rootData)
+        await this.R.docRef.set(this.R.data)
+        this.R.changeMade = false
     }
 }
 // TODO:
@@ -45,18 +66,14 @@ export class FireCursor<Root extends Objectish, Sub = Root> {
 //         rootCursorInstance: FireCursor
 //     }
 // }
+let rootInstance: FireCursor<Objectish> | null = null
 
 export async function makeRootFireCursor<Root>(docRef: firestore.DocumentReference<Root>): Promise<FireCursor<Root, Root>> {
-    // @ts-ignore
-    if (global.rootCursorInstance != null) {
-        // TODO: delete old fields from global.rootCursorInstance before object.assign
-        // @ts-ignore
-        Object.assign(global.rootCursorInstance.rootData, (await docRef.get()).data())
-        // @ts-ignore
-        return global.rootCursorInstance
+    if (rootInstance != null) {
+        Object.assign(rootInstance.R.data, (await docRef.get()).data())
+        return rootInstance as FireCursor<Root, Root>
     }
-    // @ts-ignore
-    global.rootCursorInstance = new FireCursor(docRef, [], (await docRef.get()).data())
-    // @ts-ignore
-    return global.rootCursorInstance
+    const cursorRoot = new CursorRoot<Root>(docRef, (await docRef.get()).data() as Root, false)
+    rootInstance = new FireCursor(cursorRoot, [])
+    return rootInstance as FireCursor<Root, Root>
 }
