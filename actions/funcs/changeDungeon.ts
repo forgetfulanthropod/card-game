0b9211f333faff0e14ac26920074005a8f066aa7
Charlie C Miles <charliecmiles@gmail.com>
2021-10-21 15:00:12 -0400

    fixed infinite recursion changeDungeon copy/paste typo thing

--- actions/funcs/changeDungeon.ts
+++ actions/funcs/changeDungeon.ts
@@ -1,3 +1,6 @@
 import { onCallWrapper } from '../util/onCallWrapper'
 
-export default onCallWrapper(function changeDungeon(args: { direction: -1 | 1 }) { changeDungeon(args) })
+
+import { changeDungeon as changeDungeon_ } from '../gameState/entry/actions'
+
+export default onCallWrapper(function changeDungeon(args: { direction: -1 | 1 }) { changeDungeon_(args) })
