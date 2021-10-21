a07f6880623c1f703bf7def27c71399734da5336
Luke Harold Miles <luke@cs.uky.edu>
2021-10-21 07:39:54 -0400

    minimal socket.io thing

--- actions/index.ts
+++ actions/index.ts
@@ -1,8 +1,12 @@
 //@ts-ignore
 import _ from 'ts-node/register' // debugging
 import express from 'express'
-// @ts-ignore
+import { Server as SocketServer } from 'socket.io'
+import { Server as HttpServer } from 'http'
 const app = express()
+const http = new HttpServer(app)
+const io = new SocketServer(http)
+// @ts-ignore
 
 export function getApp() {
     return app
@@ -10,15 +14,38 @@ export function getApp() {
 
 const port = 3002
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html>
+       <head>
+          <title>Hello world</title>
+       </head>
+       <script src = "/socket.io/socket.io.js"></script>
+
+       <script>
+          const socket = io();
+          socket.on('hey', () => console.log('received hey'))
+       </script>
+       <body>Hello</body>
+    </html>
+    `)
+})
 // app.get('/hi', (req, res) => {
 //     res.send('hi there')
 // })
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+    console.log('A user connected')
+    setTimeout(() => { socket.emit('hey') }, 1000)
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+        console.log('A user disconnected')
+    })
+})
 
 import { doIt } from './functions'
 doIt()
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+http.listen(port, function () {
+    console.log(`listening on port ${port}`)
 })
