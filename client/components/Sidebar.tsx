7fc132c7629870f7a4cc0aa4329db47182b572f8
Luke Harold Miles <luke@cs.uky.edu>
2021-10-25 18:12:28 -0400

    json editor seems to work well enough

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,15 +1,53 @@
+import type { Gamestate, MyCursor } from '@shared'
 import { h, JSX } from 'preact' // eslint-disable-line
-import { getClientTree, getTree } from '@/data/rootTree'
 import { useState } from 'preact/hooks'
-
+import JSONInput from 'react-json-editor-ajrm'
+// @ts-ignore
+import locale from 'react-json-editor-ajrm/locale/en'
 // @ts-ignore
 import styled from 'styled-components'
-import type { MyCursor } from '@shared'
+
+import { getClientTree, getTree } from '@/data/rootTree'
+
+
+interface JSONEditData {
+    plainText: unknown
+    markupText: unknown
+    json: unknown
+    jsObject: unknown
+    lines: unknown
+    error: unknown
+}
+
 export function Sidebar(): JSX.Element {
-    return <>
+    return <div style={{ pointerEvents: 'auto' }}>
         <OneSidebar cursor={getTree().select('events')} title="Event History" top="0" />
         <OneSidebar cursor={getClientTree().select('serverCalls')} title="Server calls" top="1em" />
-    </>
+        <GamestateEditor top="2em" />
+    </div>
+}
+
+function GamestateEditor(props: { top: string }): JSX.Element {
+    const [data, setData] = useState(getTree().get())
+    getTree().on('update', () => { setData(getTree().get()) })
+    const [shown, setShown] = useState(false)
+    const title = 'Edit gamestate tree'
+    return <Root shown={shown} top={props.top} styleChildren={false}>
+        <span onClick={() => setShown(s => !s)}>
+            {shown ? <b>{title}</b> : title}
+        </span>
+        {shown &&
+            <JSONInput
+                id='gamestate-editor'
+                theme='light_mitsuketa_tribute'
+                locale={locale}
+                placeholder={data}
+                height='100%'
+                onChange={(data: JSONEditData) => {
+                    getTree().set(data.jsObject as Gamestate)
+                }}
+            />}
+    </Root>
 }
 
 function OneSidebar<T>(props: { cursor: MyCursor<T[]>, title: string, top: string }): JSX.Element {
@@ -17,7 +55,7 @@ function OneSidebar<T>(props: { cursor: MyCursor<T[]>, title: string, top: strin
     const [shown, setShown] = useState(false)
     const [events, setEvents] = useState(c.get())
     c.on('update', () => setEvents(c.get()))
-    return <Root shown={shown} top={props.top}>
+    return <Root shown={shown} top={props.top} styleChildren={true}>
         <span onClick={() => setShown(s => !s)}>
             {shown ? <b>{props.title}</b> : props.title}
         </span>
@@ -28,7 +66,7 @@ function OneSidebar<T>(props: { cursor: MyCursor<T[]>, title: string, top: strin
     </Root>
 }
 
-interface RP { shown: boolean, top: string }
+interface RP { shown: boolean, top: string, styleChildren: boolean }
 const Root = styled.div`
     pointer-events: auto;
     background: white;
@@ -38,12 +76,14 @@ const Root = styled.div`
     right: 0;
     top: ${(p: RP) => p.top};
     height: ${(p: RP) => p.shown ? '100%' : ''};
-    >div {
-        height: 100%;
-        >div {
-            border: 1px solid black;
-            margin: 5px;
-            padding: 5px;
-        }
+    ${(p: RP) => p.styleChildren &&
+        `>div {
+            height: 100%;
+            >div {
+                border: 1px solid black;
+                margin: 5px;
+                padding: 5px;
+            }
+        }`
     }
 `
