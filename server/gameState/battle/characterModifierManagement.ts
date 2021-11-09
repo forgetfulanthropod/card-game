4fcfe4834c943f4f7e32249517d954ef051b69ac
Charlie C Miles <charliecmiles@gmail.com>
2021-11-09 15:00:45 -0500

    damage ranges updating properly

--- server/gameState/battle/characterModifierManagement.ts
+++ server/gameState/battle/characterModifierManagement.ts
@@ -4,32 +4,33 @@ import { newNPCMeta, newPCMeta } from '.'
 import { getCharacterMovesWithDamageRanges } from './attack'
 import { applyBlessings } from './blessings'
 
-/** Returns updated blessing. Does not modify in place! (i.e. pure function) (That's the goal at least.) */
-
 
 export function getModified(prev: Readonly<CharacterMeta>): CharacterMeta {
-    // TODO: should probably make brand new info from stance + a key in the statsMap
-    const clean = cleanMetaFromStats(prev)
+    const clean = cleanMeta(prev)
 
     const blessed = applyBlessings(clean)
     const stanced = { ...blessed, moves: getCharacterMovesWithDamageRanges(blessed) }
 
-    const final = copyDynamicProperties(stanced, prev)
+    const final = copyFinalProperties(stanced, prev)
 
     return final
 }
 
-function cleanMetaFromStats(cm: Readonly<CharacterMeta>): CharacterMeta {
-    return cm.isPc ? newPCMeta(cm) : newNPCMeta(cm)
+function cleanMeta(cm: Readonly<CharacterMeta>): CharacterMeta {
+    const character = cm.isPc ? newPCMeta(cm) : newNPCMeta(cm)
+
+    return {
+        ...character,
+        stance: cm.stance,
+        effects: cm.effects,
+    }
 }
 
-function copyDynamicProperties(to: Readonly<CharacterMeta>, from: CharacterMeta): CharacterMeta {
+function copyFinalProperties(to: Readonly<CharacterMeta>, from: CharacterMeta): CharacterMeta {
     const deltaHealth = to.maxHealth - from.maxHealth
     const newHealth = Math.max(from.health + deltaHealth, 1)
     return {
         ...to,
-        effects: from.effects,
-        stance: from.stance,
         health: newHealth,
         hasMoved: from.hasMoved,
         experience: from.experience,
