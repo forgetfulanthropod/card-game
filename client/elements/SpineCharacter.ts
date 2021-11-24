fa15ad593a9bf80c92dcf4e8b24ed9b9bdc48001
Charlie C Miles <charliecmiles@gmail.com>
2021-11-24 12:12:49 -0500

    ugly frog kind of works no animation

--- client/elements/SpineCharacter.ts
+++ client/elements/SpineCharacter.ts
@@ -5,7 +5,9 @@ import type { PixiApplication, PixiLoader } from '@/elementsUtil'
 export function addSpineCharacter(app: PixiApplication): void {
 
     app.loader
-        .add('spineboypro', 'spine/spineboy-pro2.json')
+        // .add('spineboypro', 'spine/demo/spineboy-pro.json')
+        // .add('spineboypro', 'spine/penguin/spine.json')
+        .add('spineboypro', 'spine/frog/FK_RIG_007.json')
         .load(onAssetsLoaded)
 
     app.stage.interactive = true
@@ -22,22 +24,22 @@ export function addSpineCharacter(app: PixiApplication): void {
 
         app.stage.addChild(spineBoyPro)
 
-        const singleAnimations = ['aim', 'death', 'jump', 'portal']
-        const loopAnimations = ['hoverboard', 'idle', 'run', 'shoot', 'walk']
-        const allAnimations = [...singleAnimations, ...loopAnimations]
+        // const singleAnimations = ['aim', 'death', 'jump', 'portal']
+        // const loopAnimations = ['hoverboard', 'idle', 'run', 'shoot', 'walk']
+        // const allAnimations = [...singleAnimations, ...loopAnimations]
 
-        let lastAnimation = ''
+        // let lastAnimation = ''
 
-        // Press the screen to play a random animation
-        app.stage.on('pointerdown', () => {
-            let animation = ''
-            do {
-                animation = allAnimations[Math.floor(Math.random() * allAnimations.length)]
-            } while (animation === lastAnimation)
+        // // Press the screen to play a random animation
+        // app.stage.on('pointerdown', () => {
+        //     let animation = ''
+        //     do {
+        //         animation = allAnimations[Math.floor(Math.random() * allAnimations.length)]
+        //     } while (animation === lastAnimation)
 
-            spineBoyPro.state.setAnimation(0, animation, loopAnimations.includes(animation))
+        //     spineBoyPro.state.setAnimation(0, animation, loopAnimations.includes(animation))
 
-            lastAnimation = animation
-        })
+        //     lastAnimation = animation
+        // })
     }
 }
