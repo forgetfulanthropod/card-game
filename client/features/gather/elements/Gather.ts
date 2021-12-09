ec48ecc343a29df7b7280994342e6a0b06c0874b
Luke Harold Miles <lukem@sent.com>
2021-12-09 14:17:46 -0800

    two squares

--- client/features/gather/elements/Gather.ts
+++ client/features/gather/elements/Gather.ts
@@ -1,35 +1,42 @@
-import type { PixiContainer } from '@/elementsUtil'
+import type { PixiContainer, PixiSprite } from '@/elementsUtil'
 import { Container, PixiTexture, Sprite } from '@/elementsUtil'
 
 export function Gather(): PixiContainer {
-    const square = Sprite({
-        src: PixiTexture.WHITE,
-        // onClick: () => alert('clicked'),
-        width: 300,
-        height: 500,
-        x: 700,
-        y: 200,
+    const makeSquare = () =>
+        makeDraggable(
+            Sprite({
+                src: PixiTexture.WHITE,
+                // onClick: () => alert('clicked'),
+                width: 300,
+                height: 500,
+                x: 600,
+                y: 200,
+            })
+        )
+    return Container({
+        children: [makeSquare(), makeSquare()],
     })
-    square.interactive = true
-    square.on('pointerdown', function (e) {
-        console.log('Picked up')
+}
 
-        square.offX = e.data.global.x - square.x
-        square.offY = e.data.global.y - square.y
-        square.dragging = true
+function makeDraggable(sprite: PixiSprite): PixiSprite {
+    let offX = 0
+    let offY = 0
+    let dragging = false
+
+    sprite.interactive = true
+    sprite.on('pointerdown', e => {
+        offX = e.data.global.x - sprite.x
+        offY = e.data.global.y - sprite.y
+        dragging = true
     })
-    square.on('pointermove', function (e) {
-        // console.log('Dragging')
-
-        if (square.dragging) {
-            square.x = e.data.global.x - square.offX
-            square.y = e.data.global.y - square.offY
+    sprite.on('pointermove', e => {
+        if (dragging) {
+            sprite.x = e.data.global.x - offX
+            sprite.y = e.data.global.y - offY
         }
     })
-    square.on('pointerup', function (e) {
-        square.dragging = false
+    sprite.on('pointerup', () => {
+        dragging = false
     })
-    return Container({
-        children: [square],
-    })
+    return sprite
 }
