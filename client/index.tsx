4fabc0ac86cb41b02f22519b32c44cf397f51d14
Luke Harold Miles <lukem@sent.com>
2021-12-08 17:37:07 -0800

    draw a circle

--- client/index.tsx
+++ client/index.tsx
@@ -1,3 +1,21 @@
+// import { render } from 'preact'
+
+// import App from './components/App'
+import { Application, Graphics } from './elementsUtil'
+
+const app = Application({ canvas: document.getElementById('pixi-root') as HTMLCanvasElement, children: [] })
+app.stage.addChild(
+    Graphics({
+        draw: g => {
+            g.beginFill(0xff0000)
+            g.drawCircle(0, 0, 100)
+            g.endFill()
+        },
+    })
+)
+// render(<App />, document.getElementById('preact-root') as HTMLDivElement)
+
+/*
 import './global.css'
 import './util/windowUtils'
 
@@ -23,8 +41,10 @@ log('client build info:', {
 void hello().then(res => log('hello got', res))
 
 async function main() {
+    maybeMakeSocket()
     await Promise.all([resolveWhenSocketConfirmed(), loadAssets()])
 
     render(<App />, document.getElementById('preact-root') as HTMLDivElement)
 }
 void main()
+ */
