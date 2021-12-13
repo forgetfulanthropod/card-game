98c8f059d8b680be085b4c49d5992254387e5f8b
Luke Harold Miles <lukem@sent.com>
2021-12-13 14:54:49 -0800

    make all the buttons for the pages

--- client/components/TempApp.tsx
+++ client/components/TempApp.tsx
@@ -1,14 +1,29 @@
-import { Button, ChakraProvider } from '@chakra-ui/react'
+import { Button, ChakraProvider, Heading, SimpleGrid } from '@chakra-ui/react'
 import { h } from 'preact' // eslint-disable-line
 
 export function TempApp(): JSX.Element {
     return (
-        <div>
-            <h1>This is a header</h1>
-            <button>Ordinary button</button>
-            <ChakraProvider>
-                <Button colorScheme="blue">Chakra button</Button>
-            </ChakraProvider>
-        </div>
+        <ChakraProvider>
+            <Heading>Kaiju Cards Wireframes!</Heading>
+            <SimpleGrid columns={2} spacing={10} margin={'20px'}>
+                <MyButton text={'battle'} />
+                <MyButton text={'craft'} />
+                <MyButton text={'spawn'} />
+                <MyButton text={'gather idle'} />
+                <MyButton text={'gather minigame'} />
+                <MyButton text={'marketplace'} />
+                <MyButton text={'skill tree'} />
+                <MyButton text={'refining'} />
+                <MyButton text={'lending'} />
+                <MyButton text={'wallet tools'} />
+            </SimpleGrid>
+        </ChakraProvider>
+    )
+}
+function MyButton(props: { text: string }) {
+    return (
+        <Button colorScheme="blue" height="90px" fontSize={'2em'}>
+            {props.text}
+        </Button>
     )
 }
