f2de09b17190b9961b7fbefd7a44999233ce55e1
Luke Harold Miles <lukem@sent.com>
2021-12-17 12:17:39 -0800

    arrow keys working pretty well

--- client/features/overworld/components/attachViewport.ts
+++ client/features/overworld/components/attachViewport.ts
@@ -6,6 +6,11 @@ import { Application, Sprite } from '@/elementsUtil'
 import mapPng from '../assets/map.png'
 import arrowPng from '../assets/worn-arrow.png'
 
+// const black = 0x000000
+// const red = 0xff0000
+// const green = 0x00ff00
+// const blue = 0x0000ff
+
 export function attachViewport(props: { canvas: HTMLCanvasElement }): void {
     console.log('attempting to attach viewport')
     const app = Application({ canvas: props.canvas, children: [] })
@@ -22,13 +27,30 @@ export function attachViewport(props: { canvas: HTMLCanvasElement }): void {
 
     // add the viewport to the stage
     app.stage.addChild(viewport)
+    // png points down
+    // order is bottom, left, top, right
+    const offset = 100
+    const spacing = 40
+    const moveDist = 20
+    const keys = ['bottom', 'left', 'top', 'right'] as const
     const arrows = range(4).map(i => {
         console.log('adding sprite at angle', 90 * i)
         return Sprite({
             src: arrowPng,
-            angle: 90 * i,
+            angle: i * 90,
+            // tint: [red, green, green, blue][i],
+            scale: 0.1,
+            x: offset + [0, -spacing, 0, spacing][i],
+            y: offset + [spacing, 0, -spacing, 0][i],
+            anchor: 0.5,
+            onClick: () => {
+                const key = keys[i]
+                viewport[key] = viewport[key] + moveDist * [-1, 1, 1, -1][i]
+            },
         })
     })
+    arrows[0].tint = 0xff0000
+
     for (const a of arrows) {
         app.stage.addChild(a)
     }
