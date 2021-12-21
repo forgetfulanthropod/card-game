368ea36a7349e76c604b54be9dd4c3147cf99332
Luke Harold Miles <lukem@sent.com>
2021-12-20 18:25:21 -0800

    simplify logic a lot

--- client/features/craft/mc/Minecrafter.tsx
+++ client/features/craft/mc/Minecrafter.tsx
@@ -11,9 +11,12 @@ import { Fragment, h } from 'preact'
 import { useState } from 'preact/hooks'
 
 import { pairs } from './data'
-import { addItemToInventory, selectCraftTable } from './minecrafting'
+import { clickTableCell, doCraft } from './minecrafting'
 
 export type PSetter<T> = (cb: T | ((old: T) => T)) => void
+
+const instructions =
+    'Click on an ingredient from your inventory, then click on a cell of your crafting table to place this ingredient.'
 
 export default function Minecrafter(): JSX.Element {
     const [inventory, setInventory] = useState(() => {
@@ -29,17 +32,13 @@ export default function Minecrafter(): JSX.Element {
             <h1>Crafting Table</h1>
             <div id="grid">
                 {range(9).map(i => (
-                    <div
-                        class="gridCell"
-                        onClick={() => selectCraftTable(craftTable, i, selected, setCraftTable, setResult)}
-                        key={i}
-                    >
+                    <div class="gridCell" onClick={() => clickTableCell(i, selected, setCraftTable, setResult)} key={i}>
                         <img src={imageOf(craftTable[i])} />
                     </div>
                 ))}
             </div>
             <div class="arrow">{'➔'}</div>
-            <div id="result" onClick={() => addItemToInventory(result?.[1] ?? 0, inventory, setInventory)}>
+            <div id="result" onClick={() => doCraft(result?.[1] ?? 0, inventory, setInventory)}>
                 {result != null && (
                     <>
                         <img src={imageOf(result[1])} />
@@ -74,8 +73,6 @@ export default function Minecrafter(): JSX.Element {
     )
 }
 
-const instructions =
-    'Click on an ingredient from your inventory, then click on a cell of your crafting table to place this ingredient.'
 function imageOf(id: number): string {
     return `http://www.101computing.net/mc/${id}-0.png`
 }
--- client/features/craft/mc/minecrafting.ts
+++ client/features/craft/mc/minecrafting.ts
@@ -3,48 +3,34 @@ import { isEqual } from 'lodash'
 import { recipes } from './data'
 import type { PSetter } from './Minecrafter'
 
-/** Add newly crafted item to the inventory (if it's not already there) */
-export function addItemToInventory(newItem: number, inventory: number[], setInventory: PSetter<number[]>): void {
+export function doCraft(newItem: number, inventory: readonly number[], setInventory: PSetter<number[]>): void {
     if (newItem === 0) return
-    if (inventory.indexOf(newItem) !== -1) {
-        alert('This item is already in your inventory!')
-        return
-    }
-    const i = inventory.indexOf(0)
-    if (i === -1) {
-        alert('Inventory is full!')
-        return
-    }
+    if (inventory.includes(newItem)) return
+    const emptySlot = inventory.indexOf(0)
+    if (emptySlot === -1) return
     setInventory(inv => {
-        const A = inv.slice()
-        A[i] = newItem
-        return A
+        const a = inv.slice()
+        a[emptySlot] = newItem
+        return a
     })
 }
 
-/** compare the craft table with all recipes to see if an item can be crafted */
-function craft(craftTable: number[], setResult: PSetter<null | [string, number]>) {
-    const i = recipes.findIndex(recipe => isEqual(recipe[2], craftTable))
-    if (i === -1) {
-        setResult(null)
-        return
-    }
-    setResult([recipes[i][0], recipes[i][1]])
-}
-
 /** Replace ingredient on craft table when the user click on one of the 9 cells of the craft table */
-export function selectCraftTable(
-    craftTable: number[],
-    cell_ID: number,
+export function clickTableCell(
+    cellIndex: number,
     selectedIngredient: number,
     setCraftTable: PSetter<number[]>,
     setResult: PSetter<null | [string, number]>
 ): void {
     setCraftTable(ct => {
         const A = ct.slice()
-        A[cell_ID] = A[cell_ID] === 0 ? selectedIngredient : 0
+        A[cellIndex] = A[cellIndex] !== selectedIngredient ? selectedIngredient : 0
+        findRecipe(A, setResult)
         return A
     })
-    craftTable[cell_ID] = selectedIngredient
-    craft(craftTable, setResult)
+}
+
+function findRecipe(craftTable: readonly number[], setResult: PSetter<null | [string, number]>) {
+    const i = recipes.findIndex(recipe => isEqual(recipe[2], craftTable))
+    setResult(i === -1 ? null : [recipes[i][0], recipes[i][1]])
 }
