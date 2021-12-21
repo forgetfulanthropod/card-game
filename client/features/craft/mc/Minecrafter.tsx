dd26242112a2c2b75599b767894c85e36e5cab50
Luke Harold Miles <lukem@sent.com>
2021-12-20 18:38:39 -0800

    more tidying

--- client/features/craft/mc/Minecrafter.tsx
+++ client/features/craft/mc/Minecrafter.tsx
@@ -8,8 +8,8 @@ import { range } from 'lodash'
 import { Fragment, h } from 'preact'
 import { useState } from 'preact/hooks'
 
-import { pairs } from './data'
-import { clickTableCell, doCraft } from './minecrafting'
+import { initialInventory } from './data'
+import { clickTableCell, doCraft } from './logic'
 import s from './style.module.css'
 
 export type PSetter<T> = (cb: T | ((old: T) => T)) => void
@@ -18,22 +18,18 @@ const instructions =
     'Click on an ingredient from your inventory, then click on a cell of your crafting table to place this ingredient.'
 
 export default function Minecrafter(): JSX.Element {
-    const [inventory, setInventory] = useState(() => {
-        const A = range(27).map(() => 0)
-        pairs.forEach(([_, x], i) => (A[i] = x))
-        return A
-    })
+    const [inventory, setInventory] = useState(initialInventory)
     const [result, setResult] = useState<null | [string, number]>(null)
     const [selected, setSelected] = useState(inventory[0])
     const [craftTable, setCraftTable] = useState(range(9).map(() => 0))
     return (
-        <div class={s['cssFullReset']}>
-            <div class={s['screen']}>
+        <div class={s.cssFullReset}>
+            <div class={s.screen}>
                 <h1>Crafting Table</h1>
-                <div class={s['grid']}>
+                <div class={s.grid}>
                     {range(9).map(i => (
                         <div
-                            class={s['gridCell']}
+                            class={s.gridCell}
                             onClick={() => clickTableCell(i, selected, setCraftTable, setResult)}
                             key={i}
                         >
@@ -41,8 +37,8 @@ export default function Minecrafter(): JSX.Element {
                         </div>
                     ))}
                 </div>
-                <div class={s['arrow']}>{'➔'}</div>
-                <div class={s['result']} onClick={() => doCraft(result?.[1] ?? 0, inventory, setInventory)}>
+                <div class={s.arrow}>{'➔'}</div>
+                <div class={s.result} onClick={() => doCraft(result?.[1] ?? 0, inventory, setInventory)}>
                     {result != null && (
                         <>
                             <img src={imageOf(result[1])} />
@@ -56,13 +52,13 @@ export default function Minecrafter(): JSX.Element {
 
                 <h1>Inventory</h1>
                 <p>{instructions}</p>
-                <div class={s['inventory']}>
+                <div class={s.inventory}>
                     {range(27).map(i =>
                         inventory[i] === 0 ? (
-                            <div class={s['gridCell']} key={i}></div>
+                            <div class={s.gridCell} key={i}></div>
                         ) : (
                             <div
-                                class={s['gridCell']}
+                                class={s.gridCell}
                                 onClick={() => setSelected(inventory[i])}
                                 style={{ backgroundColor: selected === inventory[i] ? '#88FF88' : '#8b8b8b' }}
                                 key={i}
