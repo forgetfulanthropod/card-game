77240b33508c725327dbaa5806fd8f2bd1962362
Luke Harold Miles <lukem@sent.com>
2021-12-21 15:45:20 -0800

    images shown

--- client/features/marketplace/components/CharacterDetailPage.tsx
+++ client/features/marketplace/components/CharacterDetailPage.tsx
@@ -1,15 +1,20 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Box, Heading, Image, Text } from '@chakra-ui/react'
 import type { JSX } from 'preact'
-import { Fragment, h } from 'preact'
+import { h } from 'preact'
 
 import { PageHeader } from '@/components/PageHeader'
 import type { PageProps } from '@/components/PageManager'
+
+import { ChainLightning, Dispel, MagicMirrorV2 } from '../assets/Wizard'
 export default function CharacterDetailPage(props: PageProps): JSX.Element {
     return (
-        <>
+        <Box overflowY={'scroll'} position="fixed" top={0} left={0} right={0} bottom={0}>
             <Heading>Marketplace Character Details</Heading>
             <PageHeader setPage={props.setPage} />
             <Text>Character details go here</Text>
-        </>
+            <Image src={ChainLightning} />
+            <Image src={Dispel} />
+            <Image src={MagicMirrorV2} />
+        </Box>
     )
 }
