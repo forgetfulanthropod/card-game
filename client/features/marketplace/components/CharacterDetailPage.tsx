c751af87a179c4f6c4df026dcf7d8acc0506ad24
Luke Harold Miles <lukem@sent.com>
2021-12-21 17:20:33 -0800

    add certs

--- client/features/marketplace/components/CharacterDetailPage.tsx
+++ client/features/marketplace/components/CharacterDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Circle, Grid, GridItem, Heading, Image, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Circle, Grid, GridItem, Heading, HStack, Image, SimpleGrid, Square, Text } from '@chakra-ui/react'
 import { useMediaQuery } from '@chakra-ui/react'
 import { random } from 'lodash'
 import type { JSX } from 'preact'
@@ -36,9 +36,9 @@ const imageSrcs = [
 ]
 
 const icons = ['👍', '🎉', '💪', '🏃‍♀️', '🧠', '👀', '🍀']
-
+const classes = ['Trinity-Cleric', 'Climber-Arborist']
+const certs = ['🚗', '🪚', '✂️']
 export default function CharacterDetailPage(props: PageProps): JSX.Element {
-    // const [order, setOrder] = useState(imageSrcs)
     const [isLarge] = useMediaQuery('(min-width: 62em)')
 
     const cards = (
@@ -54,16 +54,57 @@ export default function CharacterDetailPage(props: PageProps): JSX.Element {
     const character = (
         <GridItem colSpan={[4, 4, 4, 2]}>
             <Text>My funKYy Penguin</Text>
+            {classes.map(cl => (
+                <Text key={cl} margin="10px" as="span" bg="#333" fontSize={'1.5rem'} p="8px" borderRadius="12px">
+                    {cl}
+                </Text>
+            ))}
             <Image width={'100%'} src={characterSrc} />
+        </GridItem>
+    )
+    const gatherCraft = (
+        <GridItem colSpan={[4, 4, 4, 1]}>
+            <Text>Gather / Craft</Text>
+            <SimpleGrid columns={4} spacing={1}>
+                {icons.flatMap(icon => [
+                    <Box key={icon} textAlign="center">
+                        <Circle size="50px" bg="white">
+                            {icon}
+                        </Circle>
+                    </Box>,
+                    <Text key={icon}>{random(1, 10)}</Text>,
+                ])}
+            </SimpleGrid>
+            <Text>Certs</Text>
+            <HStack>
+                {certs.map((cert, i) => (
+                    <Square
+                        // display="inline-block"
+                        // as="span"
+                        key={i}
+                        size="1.5em"
+                        bg="silver"
+                        borderRadius="5%"
+                        // m="5"
+                        // centerContent={true}
+                    >
+                        {cert}
+                    </Square>
+                ))}
+            </HStack>
         </GridItem>
     )
     return (
         <>
-            <Heading size="2xl">Marketplace Character Details</Heading>
+            <Heading size="2xl">
+                Marketplace Character Details
+                {/* <Button onClick={() => props.setPage('MenuPage')}>Home</Button> */}
+            </Heading>
 
             <Grid
                 templateColumns="repeat(4, 1fr)"
                 columns={3}
+                gap={10}
                 overflowY={'scroll'}
                 position="fixed"
                 top="13%"
@@ -74,33 +115,9 @@ export default function CharacterDetailPage(props: PageProps): JSX.Element {
                 color="white"
                 fontSize="3rem"
             >
-                {/* <Heading>Marketplace Character Details</Heading>
-            <PageHeader setPage={props.setPage} />
-            <Text>Character details go here</Text> */}
-                {/* <VStack spacing="-80%" marginTop="50%"> */}
                 {isLarge ? cards : character}
                 {isLarge ? character : cards}
-                <GridItem colSpan={[4, 4, 4, 1]}>
-                    <Text>Gather / Craft</Text>
-                    <SimpleGrid columns={4} spacing={1}>
-                        {icons.flatMap(icon => [
-                            <Box key={icon} textAlign="center">
-                                <Circle size="50px" bg="white">
-                                    {icon}
-                                </Circle>
-                            </Box>,
-                            <Text key={icon}>{random(1, 10)}</Text>,
-                        ])}
-                    </SimpleGrid>
-                </GridItem>
-                {/* <Box w={['100vw', '50vw', '50vw']}>
-                <Image width={200} src={characterSrc} />
-            </Box> */}
-                {/* <SimpleGrid columns={2} spacing={10} w={['100vw', '50vw', '25vw']}>
-                {imageSrcs.map(src => (
-                    <Image width={200} key={src} src={src} />
-                ))}
-            </SimpleGrid> */}
+                {gatherCraft}
             </Grid>
         </>
     )
