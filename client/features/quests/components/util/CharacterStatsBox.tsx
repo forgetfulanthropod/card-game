83d7e064b09b77c57462b1e806bf0ef29e90f482
Luke Harold Miles <lukem@sent.com>
2021-12-28 22:55:30 -0800

    add CharacterStatsBox

--- client/features/quests/components/util/CharacterStatsBox.tsx
+++ client/features/quests/components/util/CharacterStatsBox.tsx
@@ -1,14 +1,32 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import type { JSX } from 'preact'
 import { h } from 'preact'
 
-export default function CharacterStatsBox(): JSX.Element {
+export default function CharacterStatsBox(props: { strength: number; rarity: string; ownedSince: Date }): JSX.Element {
+    const data: [string, any][] = [
+        ['strength', props.strength],
+        ['rarity', props.rarity],
+        ['owned since', props.ownedSince.toLocaleDateString()],
+    ]
     return (
-        <Box>
-            <Button>CharacterStatsBox</Button>
-        </Box>
+        <Table>
+            <Thead>
+                <Tr>
+                    <Th>Stat</Th>
+                    <Th>Value</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {data.map(([stat, value]) => (
+                    <Tr key={stat}>
+                        <Td>{stat}</Td>
+                        <Td>{value}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
     )
 }
 export function CharacterStatsBoxExample(): JSX.Element {
-    return <CharacterStatsBox />
+    return <CharacterStatsBox strength={5} rarity="common" ownedSince={new Date()} />
 }
