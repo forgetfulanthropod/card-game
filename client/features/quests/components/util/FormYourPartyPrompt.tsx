5839b09879d457f67ce8e83203c3368221821fff
Luke Harold Miles <lukem@sent.com>
2021-12-28 23:07:35 -0800

    improve 'form your party'

--- client/features/quests/components/util/FormYourPartyPrompt.tsx
+++ client/features/quests/components/util/FormYourPartyPrompt.tsx
@@ -5,7 +5,17 @@ import { h } from 'preact'
 export default function FormYourPartyPrompt(): JSX.Element {
     return (
         <Box>
-            <Button>FormYourPartyPrompt</Button>
+            <Button
+                fontSize="xx-large"
+                margin="1em"
+                padding="1em"
+                bg="black"
+                color="white"
+                boxShadow="10px 5px 5px gray"
+                _hover={{ bg: 'darkgray' }}
+            >
+                FORM YOUR PARTY
+            </Button>
         </Box>
     )
 }
