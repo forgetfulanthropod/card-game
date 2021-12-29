699e4ba99d8afffb908edcfcea173efa07ad82e1
Luke Harold Miles <lukem@sent.com>
2021-12-28 23:03:43 -0800

    fill out quest progress bar

--- client/features/quests/components/util/QuestProgressBar.tsx
+++ client/features/quests/components/util/QuestProgressBar.tsx
@@ -1,14 +1,15 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Heading, Progress, VStack } from '@chakra-ui/react'
 import type { JSX } from 'preact'
 import { h } from 'preact'
 
-export default function QuestProgressBar(): JSX.Element {
+export default function QuestProgressBar(props: { percent: number; title: string }): JSX.Element {
     return (
-        <Box>
-            <Button>QuestProgressBar</Button>
-        </Box>
+        <VStack bg="gray.100" p="10px" m="2px" borderRadius="2px">
+            <Heading>{props.title}</Heading>
+            <Progress value={props.percent} size="lg" width="100%" bg="gray.400" />
+        </VStack>
     )
 }
 export function QuestProgressBarExample(): JSX.Element {
-    return <QuestProgressBar />
+    return <QuestProgressBar percent={75} title="Save Eden Village" />
 }
