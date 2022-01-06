d58b895c5794741dfb1d46c90962c254d75a25eb
Luke Harold Miles <lukem@sent.com>
2022-01-06 12:28:41 -0800

    add function to read balance without react

--- client/components/WalletWidget.tsx
+++ client/components/WalletWidget.tsx
@@ -3,15 +3,6 @@ import '@solana/wallet-adapter-react-ui/styles.css'
 
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import {
-    ConnectionProvider,
-    WalletProvider,
-} from '@solana/wallet-adapter-react'
-import {
-    WalletDisconnectButton,
-    WalletModalProvider,
-    WalletMultiButton,
-} from '@solana/wallet-adapter-react-ui'
-import {
     // LedgerWalletAdapter,
     PhantomWalletAdapter,
     // SlopeWalletAdapter,
@@ -20,10 +11,10 @@ import {
     // SolletWalletAdapter,
     // TorusWalletAdapter,
 } from '@solana/wallet-adapter-wallets'
-import { clusterApiUrl } from '@solana/web3.js'
-import { useMemo } from 'preact/hooks'
+import { clusterApiUrl, Connection } from '@solana/web3.js'
+import { useEffect } from 'preact/hooks'
 
-export default function WalletWidget() {
+/* export default function WalletWidget() {
     // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
     const network = WalletAdapterNetwork.Devnet
 
@@ -46,6 +37,8 @@ export default function WalletWidget() {
         []
         // [network]
     )
+    const phantom = wallets[0]
+    phantom.on('connect')
 
     return (
         <ConnectionProvider endpoint={endpoint}>
@@ -57,4 +50,40 @@ export default function WalletWidget() {
             </WalletProvider>
         </ConnectionProvider>
     )
+} */
+
+export default function WalletWidget() {
+    useEffect(() => {
+        void foo()
+    }, [])
+    return <h2>Wallet widget</h2>
+}
+
+async function foo() {
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Devnet
+
+    // You can also provide a custom RPC endpoint
+    const endpoint = clusterApiUrl(network)
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded
+    const wallet = new PhantomWalletAdapter()
+    const connection = new Connection(endpoint)
+    // wallet.
+    await new Promise(resolve =>
+        wallet.once('readyStateChange', s => {
+            console.log('ready state:', s)
+            resolve(null)
+        })
+    )
+    await wallet.connect()
+    console.log('public key:', wallet.publicKey)
+    if (wallet.publicKey == null) {
+        throw Error('null public key')
+    }
+    console.log('balance:', await connection.getBalance(wallet.publicKey))
+    console.log('wallet is connected')
+    // const pk: PublicKey = await new Promise(resolve => wallet.on('connect', pk => resolve(pk)))
 }
