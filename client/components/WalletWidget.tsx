9ce87c1d12c9430f94f5afc818ce495333f00e77
Luke Harold Miles <lukem@sent.com>
2022-01-07 12:19:26 -0800

    get token metadata and display image

--- client/components/WalletWidget.tsx
+++ client/components/WalletWidget.tsx
@@ -1,18 +1,11 @@
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css'
 
+import { Metadata } from '@metaplex-foundation/mpl-token-metadata'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import {
-    // LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    // SlopeWalletAdapter,
-    // SolflareWalletAdapter,
-    // SolletExtensionWalletAdapter,
-    // SolletWalletAdapter,
-    // TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets'
-import { clusterApiUrl, Connection } from '@solana/web3.js'
-import { useEffect } from 'preact/hooks'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js'
+import { useEffect, useState } from 'preact/hooks'
 
 /* export default function WalletWidget() {
     // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
@@ -53,25 +46,25 @@ import { useEffect } from 'preact/hooks'
 } */
 
 export default function WalletWidget() {
+    const [image, setImage] = useState<string | null>(null)
     useEffect(() => {
-        void foo()
+        void getNftImage().then(image => setImage(image))
+        // void bar()
     }, [])
-    return <h2>Wallet widget</h2>
+    return (
+        <>
+            <h2>Wallet widget</h2>
+            {image != null && <img src={image} />}
+        </>
+    )
 }
 
-async function foo() {
+async function getNftImage(): Promise<string> {
     // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-    const network = WalletAdapterNetwork.Devnet
-
-    // You can also provide a custom RPC endpoint
-    const endpoint = clusterApiUrl(network)
-
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-    // Only the wallets you configure here will be compiled into your application, and only the dependencies
-    // of wallets that your users connect to will be loaded
+    const connection = new Connection(
+        clusterApiUrl(WalletAdapterNetwork.Mainnet)
+    )
     const wallet = new PhantomWalletAdapter()
-    const connection = new Connection(endpoint)
-    // wallet.
     await new Promise(resolve =>
         wallet.once('readyStateChange', s => {
             console.log('ready state:', s)
@@ -83,7 +76,56 @@ async function foo() {
     if (wallet.publicKey == null) {
         throw Error('null public key')
     }
-    console.log('balance:', await connection.getBalance(wallet.publicKey))
-    console.log('wallet is connected')
-    // const pk: PublicKey = await new Promise(resolve => wallet.on('connect', pk => resolve(pk)))
+    // console.log('balance:', await connection.getBalance(wallet.publicKey))
+    // console.log(
+    //     'getTokenAccountsByOwner:',
+    //     await connection.getTokenAccountsByOwner(wallet.publicKey, {
+    //         programId: new PublicKey(
+    //             'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+    //         ),
+    //     })
+    // )
+    const accounts = await connection.getParsedProgramAccounts(
+        new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'), // TOKEN_PROGRAM_ID,
+        {
+            filters: [
+                {
+                    dataSize: 165, // number of bytes
+                },
+                {
+                    memcmp: {
+                        offset: 32, // number of bytes
+                        bytes: wallet.publicKey.toBase58(), // base58 encoded string
+                    },
+                },
+            ],
+        }
+    )
+    console.log({ accounts })
+    // @ts-expect-error
+    const mint = accounts[0].account.data.parsed.info.mint
+    console.log({ mint })
+    const METAPLEX_ADDRESS =
+        'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s' as const
+    const metaplexPub = new PublicKey(METAPLEX_ADDRESS)
+    const [pubkey, _somenum] = await PublicKey.findProgramAddress(
+        [
+            toBuffer('metadata'),
+            metaplexPub.toBuffer(),
+            new PublicKey(mint).toBuffer(),
+        ],
+        metaplexPub
+    )
+    console.log({ pubkey: pubkey.toBase58() })
+    const ownedMetadata = await Metadata.load(connection, pubkey.toBase58()) //tokenPublicKey)
+    console.log({ ownedMetadata })
+    const uri = ownedMetadata.data.data.uri
+    console.log({ uri })
+    const fullData = await (await fetch(uri)).json()
+    console.log({ fullData })
+    return fullData.image as string
+}
+
+function toBuffer(s: string): Uint8Array {
+    return new TextEncoder().encode(s)
 }
