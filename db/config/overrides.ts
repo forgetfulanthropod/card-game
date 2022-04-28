bbf393c038327b70ad4e1a2c3909a51a9c599292
Luke Harold Miles <lukem@sent.com>
2022-04-28 12:37:05 -0700

    delete junk

--- db/config/overrides.ts
+++
@@ -1,5 +0,0 @@
-// declare global {
-//     function resolve<T = unknown>(value: T | void): T
-// }
-
-export default null
--- tests/post-square.ts
+++
@@ -1,8 +0,0 @@
-const res = await fetch(`http://localhost:3000/square`, {
-    method: 'POST',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ n: 5 }),
-})
