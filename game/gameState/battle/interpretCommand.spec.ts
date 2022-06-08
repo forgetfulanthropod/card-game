475639aa716de48eee9ffa6c5336a8c3b02d7225
Luke Harold Miles <lukem@sent.com>
2022-06-08 14:20:23 -0700

    extract runTests.ts

--- game/gameState/battle/interpretCommand.spec.ts
+++ game/gameState/battle/interpretCommand.spec.ts
@@ -1,14 +1,11 @@
-// RUN_TESTS=yes esr gameState/battle/cards/interpretCommand.spec.ts
-/* eslint-disable no-console */
-
 import { deepStrictEqual as equals, ok as truthy } from 'assert'
 import { SBaobab } from 'sbaobab'
 import type { Card, CharacterUid, Command, Gamestate, TargetType } from 'shared'
-import winston from 'winston'
 import { interpretCommand, play } from './cards'
 // @ts-ignore
 import exampleBattleScene_ from './exampleBattlescene.json'
 import { getBattleSceneIn } from '@/util'
+
 const exampleBattleScene = exampleBattleScene_ as unknown as Gamestate
 const originalScene = exampleBattleScene_.scene
 
@@ -16,8 +13,6 @@ const pc1 = 'pc-1'
 const pc2 = 'pc-2'
 const npc1 = 'npc-1'
 const npc2 = 'npc-2'
-
-global.logger = winston.createLogger()
 
 const interpretCommandSuite = {
     addBlock() {
@@ -134,13 +129,21 @@ const interpretCommandSuite = {
         truthy(scene.get('allCharacters', pc1, 'health') > 0)
         truthy(scene.get('allCharacters', pc2, 'health') <= 0)
     },
-    momentary() {},
-    orb() {},
-    queue() {},
-    text() {},
+    momentary() {
+        const scene = freshBattleScene()
+        const card: Card = makeCard(pc1, 'chain(addEnergy(2), momentary())')
+        scene.set(['cards', 'hand', card.uid], card)
+        truthy(scene.select('cards', 'hand').get(card.uid) != null)
+        play({ card, scene, targetUids: [] })
+        truthy(scene.select('cards', 'hand').get(card.uid) == null)
+        truthy(scene.select('cards', 'discard').get(card.uid) == null)
+    },
+    // orb() {},
+    // queue() {},
+    // text() {},
 } as const
 
-const effectsTest = {
+const effectsSuite = {
     bleed() {},
     debilitated() {},
     fatigue() {},
@@ -154,19 +157,23 @@ const effectsTest = {
 
 export const suites = {
     interpretCommandSuite,
-    //  effectsTest
+    //  effectsSuite
 } as const
+
+function uid() {
+    return Math.random().toString(36).slice(2)
+}
 
 function makeCard(
     owner: CharacterUid,
     actions: string,
     targetType: TargetType = 'enemies',
-    targetNum = 1
+    targetNum = 1,
+    id = uid()
 ): Card {
-    const uid = () => Math.random().toString(36).slice(2)
     return {
         // @ts-expect-error
-        id: uid(),
+        id,
         name: uid(),
         actions: actions,
         characterClass: 'knight',
@@ -201,47 +208,3 @@ function freshBattleScene() {
     const scene = getBattleSceneIn(state)
     return scene
 }
-
-function main() {
-    console.log('starting tests')
-    const suiteRatios: Record<string, string> = {}
-    for (const [suiteName, suite] of Object.entries(suites)) {
-        console.log(`\n\ntesting suite ${suiteName}`)
-        const failed: string[] = []
-        for (const [name, test] of Object.entries(suite)) {
-            console.log(`\n\ttesting ${name}`)
-            try {
-                test()
-                console.log(`✅ ${name} passed`)
-            } catch (e) {
-                console.log(`❌ ${name} failed`)
-                if (e instanceof Error) {
-                    console.log('\t\t', e.message.split('\n').join('\n\t\t'))
-                }
-                failed.push(name)
-            }
-        }
-        console.log(`finished suite ${suiteName}`)
-        const testNames = Object.keys(suite)
-        const numTests = testNames.length
-        const ratio = `${numTests - failed.length}/${numTests}`
-        suiteRatios[suiteName] = ratio
-        console.log(`\n${ratio} tests passed`)
-        console.log(`failed: ${failed.join(', ')}`)
-        console.log(
-            `passed: ${testNames
-                .filter(name => !failed.includes(name))
-                .join(', ')}`
-        )
-    }
-    console.log('\n\nsummary:')
-    for (const [name, ratio] of Object.entries(suiteRatios)) {
-        console.log(`\t${name}: ${ratio} tests passed`)
-    }
-}
-
-if (process.env.RUN_TESTS === 'yes') {
-    main()
-}
-
-/** TODO: iterate through all cards and enemies and make sure they all explain() and execute() ok */
---
+++ game/runTests.ts
@@ -0,0 +1,54 @@
+#! RUN_TESTS=yes esr runTests.ts
+/* eslint-disable no-console, import/first, import/no-internal-modules */
+import winston from 'winston'
+global.logger = winston.createLogger()
+
+type SuiteName = string
+type TestName = string
+
+const allSuites: Record<SuiteName, Record<TestName, () => void>> = {}
+// @index(['./**/*.spec.ts'], f => `import { suites as ${f.name.split('.')[0]} } from '${f.path}'\nObject.assign(allSuites, ${f.name.split('.')[0]})`)
+import { suites as interpretCommand } from './gameState/battle/interpretCommand.spec'
+Object.assign(allSuites, interpretCommand)
+// @endindex
+
+function main() {
+    console.log('starting tests')
+    const suiteRatios: Record<string, string> = {}
+    for (const [suiteName, suite] of Object.entries(allSuites)) {
+        console.log(`\n\ntesting suite ${suiteName}`)
+        const failed: string[] = []
+        for (const [name, test] of Object.entries(suite)) {
+            console.log(`\n\ttesting ${name}`)
+            try {
+                test()
+                console.log(`✅ ${name} passed`)
+            } catch (e) {
+                console.log(`❌ ${name} failed`)
+                if (e instanceof Error) {
+                    console.log('\t\t', e.message.split('\n').join('\n\t\t'))
+                }
+                failed.push(name)
+            }
+        }
+        console.log(`finished suite ${suiteName}`)
+        const testNames = Object.keys(suite)
+        const numTests = testNames.length
+        const ratio = `${numTests - failed.length}/${numTests}`
+        suiteRatios[suiteName] = ratio
+        console.log(`\n${ratio} tests passed`)
+        console.log(`failed: ${failed.join(', ')}`)
+        console.log(
+            `passed: ${testNames
+                .filter(name => !failed.includes(name))
+                .join(', ')}`
+        )
+    }
+    console.log('\n\nsummary:')
+    for (const [name, ratio] of Object.entries(suiteRatios)) {
+        console.log(`\t${name}: ${ratio} tests passed`)
+    }
+}
+if (process.env.RUN_TESTS === 'yes') {
+    main()
+}
