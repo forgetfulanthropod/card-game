e499233ec2156ed54be76cade6af525f4d41a0de
Luke Harold Miles <lukem@sent.com>
2022-06-10 14:46:07 -0700

    separate GameOnlyActions and ServerOnlyActions

--- game/actions/types/index.ts
+++ game/actions/types/index.ts
@@ -1,6 +1,8 @@
-import type { Action, Func } from 'shared'
+import type { Func, GameOnlyAction } from 'shared'
 
-export type GameActions = { [K in keyof Action]: GameAction<Action[K]> }
+export type GameActions = {
+    [K in keyof GameOnlyAction]: GameAction<GameOnlyAction[K]>
+}
 
 type GameAction<T extends Func> = (
     args: Parameters<T>[0] & { game: Gamecursor }
