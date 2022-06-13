5be1e10d5355039c281a423f21eaace90a1260d0
Luke Harold Miles <lukem@sent.com>
2022-06-13 16:25:28 -0700

    add random dungeon helper

--- game/rulebook/battle/rooms.ts
+++ game/rulebook/battle/rooms.ts
@@ -1,9 +1,13 @@
-import type { DungeonRoomMap } from 'shared'
-
+import { sample, sampleSize } from 'lodash'
+import type { DungeonRoom, DungeonRoomMap, EnemyCharacterName } from 'shared'
+import { throwNull } from 'shared/code'
+import { enemies } from '@/rulebook'
 // - Room 1:  Two level 1 Skeletons, One level 1 Matcha
 // - Room 2: Two level 1 Matchas, One level 2 Skeleton
 // - Room 3: Two Level 2 Skeletons, One Level 2 Matcha
 // - Room 4: Boss (maybe warhog)
+
+const config = { randomDungeon: false }
 
 export const dungeonRooms: DungeonRoomMap = {
     'Skelepit Dungeon': [
@@ -27,4 +31,17 @@ export const dungeonRooms: DungeonRoomMap = {
     'Fort Skeleton': [],
     'The Ninth Trash Hole of Hell': [],
     'The Matcha Caves': [],
+}
+
+if (config.randomDungeon)
+    setTimeout(() => (dungeonRooms['Skelepit Dungeon'] = randomDungeon()), 0)
+function randomDungeon(): DungeonRoom[] {
+    return Array.from({ length: 3 }, randomRoom)
+}
+function randomRoom(): DungeonRoom {
+    const names = sampleSize(Object.keys(enemies), 3) as EnemyCharacterName[]
+    return names.map(name => ({
+        name,
+        level: sample(Object.keys(enemies[name])) ?? throwNull('enemy sample'),
+    }))
 }
