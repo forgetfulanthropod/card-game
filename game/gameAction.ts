48361e35c8869f303ed2f506f3d7d9f40c75e113
Luke Harold Miles <lukem@sent.com>
2022-06-15 20:14:48 -0700

    require a requireAction

--- game/gameAction.ts
+++ game/gameAction.ts
@@ -1,9 +1,16 @@
-import type { GameActionCall, GameActions } from 'shared'
+import type { BattleCursor, GameActionCall, GameActions } from 'shared'
 import { satisfies, throwNull } from 'shared/code'
 import * as actions from './actions'
 satisfies<GameActions>(actions)
 export function doGameAction(args: GameActionCall) {
-    const method = args.method
+    const { game, method } = args
+    if (
+        game.get('scene', 'name') === 'battle' &&
+        (game.select('scene') as BattleCursor).get('requireAction') != null &&
+        method !== 'finishCard'
+    ) {
+        throw Error('you must finish your card before doing another action')
+    }
     const action = actions[method] ?? throwNull(`actions.${method}`)
     // @ts-expect-error
     action(args)
