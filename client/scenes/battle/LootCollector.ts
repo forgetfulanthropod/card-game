7ea801d62075ffad1df0c0cdb29130dada20e537
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2022-08-25 16:41:04 -0500

    added text to button

--- client/scenes/battle/LootCollector.ts
+++ client/scenes/battle/LootCollector.ts
@@ -1,12 +1,16 @@
 import { Texture } from 'pixi.js'
 
 import { InfoBox } from '@sharedElements'
-import type { PixiContainer } from '@/elementsUtil'
+import { PixiContainer, Text } from '@/elementsUtil'
 import { BASE_HEIGHT, BASE_WIDTH, Container, Sprite } from '@/elementsUtil'
 
 export function LootCollector(): PixiContainer {
+    function dismissLootCollector() {
+        console.log('testing')
+    }
+
     return Container(
-        { x: 0, y: 0, scale: 1 },
+        { x: 0, y: 0, scale: 0.5 },
         InfoBox(
             Container(
                 {},
@@ -16,9 +20,25 @@ export function LootCollector(): PixiContainer {
                     height: BASE_HEIGHT * 0.7,
                     alpha: 0.6,
                     anchor: [0.5, 0.5],
-                    x: BASE_WIDTH / 2,
-                    y: BASE_HEIGHT / 2,
-                })
+                    x: BASE_WIDTH,
+                    y: BASE_HEIGHT,
+                }),
+                Sprite({
+                    src: Texture.WHITE,
+                    width: BASE_WIDTH * 0.6,
+                    height: 100,
+                    alpha: 1,
+                    anchor: [0.5,0.5],
+                    x: BASE_WIDTH,
+                    y: BASE_HEIGHT + 500,
+                    onClick: dismissLootCollector,
+                }).addChild(Text({
+                    text: 'collect loot',
+                    anchor: [0.5,0.5],
+                    x: BASE_WIDTH,
+                    y: BASE_HEIGHT + 500,
+                    style: { fontSize: 100, fill: 'white' },
+                }))
             ),
             {
                 borderRadius: 12,
