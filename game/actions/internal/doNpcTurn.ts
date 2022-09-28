d2b36366239854bfe0037fa18ee3ac96e9d83ea6
Charlie C Miles <charliecmiles@gmail.com>
2022-09-27 17:15:02 -0700

    hotfix: game carries on if there's no npc command to process

--- game/actions/internal/doNpcTurn.ts
+++ game/actions/internal/doNpcTurn.ts
@@ -1,4 +1,4 @@
-import type { BattleScene, Command, InternalActions } from 'shared'
+import type { BattleScene, Command, Gamecursor, InternalActions } from 'shared'
 
 import {
     maybeTransitionBattleState,
@@ -17,6 +17,11 @@ export const doNpcTurn: InternalActions['doNpcTurn'] = ({ game, index }) => {
 
     logger.info(JSON.stringify({ processedCmds }))
 
+    if (processedCmds.length === 0) {
+        endNpcTurn(game)
+        return
+    }
+
     const processedCmd = processedCmds[index]
 
     const { targetUids, command } = processedCmd
@@ -25,16 +30,21 @@ export const doNpcTurn: InternalActions['doNpcTurn'] = ({ game, index }) => {
     }
 
     if (index >= processedCmds.length - 1) {
-        game.set('nextAction', {
-            delay: TIME_BETWEEN_NPC_MOVES,
-            method: 'endNpcTurn',
-        })
+        endNpcTurn(game)
         return
     }
+
     game.set('nextAction', {
         index: index + 1,
         delay: TIME_BETWEEN_NPC_MOVES,
         method: 'doNpcTurn',
+    })
+}
+
+function endNpcTurn(game: Gamecursor) {
+    game.set('nextAction', {
+        delay: TIME_BETWEEN_NPC_MOVES,
+        method: 'endNpcTurn',
     })
 }
 
