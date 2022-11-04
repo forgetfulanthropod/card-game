4929a4c1a64d5969aad9042792efb3149572f3b3
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2022-11-04 03:59:12 -0700

    added video background

--- client/components/NewStartScreen.tsx
+++ client/components/NewStartScreen.tsx
@@ -21,7 +21,13 @@ export function NewStartScreen(props: {
         onClick={() => props.onEnter('random-' + Math.random().toString())}
         class={styles.startScreenContainer}
     >
-        {/* <img class={styles.kaijuLogo} src='./logos/NewKaijuLogo.png' /> */}
+        <video
+            src='./assets/backgrounds/main_menu_shed_bg.webm'
+            autoPlay
+            loop
+            style={{maxWidth: '110%'}}
+        />
+        <img class={styles.kaijuLogo} src='./logos/NewKaijuLogo.png' />
         {showText && <p className={styles.startGame} id='startGame'>
             START GAME
         </p>}
