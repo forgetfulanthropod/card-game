658244647e8323ade8e4ff4b92c7a2a2d25d5e67
Charlie C Miles <charliecmiles@gmail.com>
2022-11-07 09:34:08 -0800

    side load heavy stuff

--- client/assets/deluxeAssetMaps.ts
+++ client/assets/deluxeAssetMaps.ts
@@ -27,3 +27,254 @@ export const soundAssets = {
     soundJerryAttack: 'sfx/Jerry Attack.mp3',
     soundJerryTakingDamage: 'sfx/Jerry Taking Damage.mp3',
 }
+
+export const videoAssets = {
+    mapBg: 'hex map/dungeon test bg for export.mp4',
+}
+
+export const sequences = {
+    // @index(['../../public/assets/fx sequences/**/*.svg'], f => `${f.name}: '${f.path.replace('../../public/assets/', '')}.svg',`)
+    KC_FX_Block_v4_001_0001:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0001.svg',
+    KC_FX_Block_v4_001_0002:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0002.svg',
+    KC_FX_Block_v4_001_0003:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0003.svg',
+    KC_FX_Block_v4_001_0004:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0004.svg',
+    KC_FX_Block_v4_001_0005:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0005.svg',
+    KC_FX_Block_v4_001_0006:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0006.svg',
+    KC_FX_Block_v4_001_0007:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0007.svg',
+    KC_FX_Block_v4_001_0008:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0008.svg',
+    KC_FX_Block_v4_001_0009:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0009.svg',
+    KC_FX_Block_v4_001_0010:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0010.svg',
+    KC_FX_Block_v4_001_0011:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0011.svg',
+    KC_FX_Block_v4_001_0012:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0012.svg',
+    KC_FX_Block_v4_001_0013:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0013.svg',
+    KC_FX_Block_v4_001_0014:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0014.svg',
+    KC_FX_Block_v4_001_0015:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0015.svg',
+    KC_FX_Block_v4_001_0016:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0016.svg',
+    KC_FX_Block_v4_001_0017:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0017.svg',
+    KC_FX_Block_v4_001_0018:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0018.svg',
+    KC_FX_Block_v4_001_0019:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0019.svg',
+    KC_FX_Block_v4_001_0020:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0020.svg',
+    KC_FX_Block_v4_001_0021:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0021.svg',
+    KC_FX_Block_v4_001_0022:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0022.svg',
+    KC_FX_Block_v4_001_0023:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0023.svg',
+    KC_FX_Block_v4_001_0024:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0024.svg',
+    KC_FX_Block_v4_001_0025:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0025.svg',
+    KC_FX_Block_v4_001_0026:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0026.svg',
+    KC_FX_Block_v4_001_0027:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0027.svg',
+    KC_FX_Block_v4_001_0028:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0028.svg',
+    KC_FX_Block_v4_001_0029:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0029.svg',
+    KC_FX_Block_v4_001_0030:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0030.svg',
+    KC_FX_Block_v4_001_0031:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0031.svg',
+    KC_FX_Block_v4_001_0032:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0032.svg',
+    KC_FX_Block_v4_001_0033:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0033.svg',
+    KC_FX_Block_v4_001_0034:
+        'fx sequences/KC_FX_Block_v3_001/KC_FX_Block_v4_001_0034.svg',
+    KC_FX_BlockBreak_v1_001_0001:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0001.svg',
+    KC_FX_BlockBreak_v1_001_0002:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0002.svg',
+    KC_FX_BlockBreak_v1_001_0003:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0003.svg',
+    KC_FX_BlockBreak_v1_001_0004:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0004.svg',
+    KC_FX_BlockBreak_v1_001_0005:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0005.svg',
+    KC_FX_BlockBreak_v1_001_0006:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0006.svg',
+    KC_FX_BlockBreak_v1_001_0007:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0007.svg',
+    KC_FX_BlockBreak_v1_001_0008:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0008.svg',
+    KC_FX_BlockBreak_v1_001_0009:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0009.svg',
+    KC_FX_BlockBreak_v1_001_0010:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0010.svg',
+    KC_FX_BlockBreak_v1_001_0011:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0011.svg',
+    KC_FX_BlockBreak_v1_001_0012:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0012.svg',
+    KC_FX_BlockBreak_v1_001_0013:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0013.svg',
+    KC_FX_BlockBreak_v1_001_0014:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0014.svg',
+    KC_FX_BlockBreak_v1_001_0015:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0015.svg',
+    KC_FX_BlockBreak_v1_001_0016:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0016.svg',
+    KC_FX_BlockBreak_v1_001_0017:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0017.svg',
+    KC_FX_BlockBreak_v1_001_0018:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0018.svg',
+    KC_FX_BlockBreak_v1_001_0019:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0019.svg',
+    KC_FX_BlockBreak_v1_001_0020:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0020.svg',
+    KC_FX_BlockBreak_v1_001_0021:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0021.svg',
+    KC_FX_BlockBreak_v1_001_0022:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0022.svg',
+    KC_FX_BlockBreak_v1_001_0023:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0023.svg',
+    KC_FX_BlockBreak_v1_001_0024:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0024.svg',
+    KC_FX_BlockBreak_v1_001_0025:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0025.svg',
+    KC_FX_BlockBreak_v1_001_0026:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0026.svg',
+    KC_FX_BlockBreak_v1_001_0027:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0027.svg',
+    KC_FX_BlockBreak_v1_001_0028:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0028.svg',
+    KC_FX_BlockBreak_v1_001_0029:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0029.svg',
+    KC_FX_BlockBreak_v1_001_0030:
+        'fx sequences/KC_FX_BlockBreak_v1_001/KC_FX_BlockBreak_v1_001_0030.svg',
+    KC_FX_Defense_v1_0010001:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010001.svg',
+    KC_FX_Defense_v1_0010002:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010002.svg',
+    KC_FX_Defense_v1_0010003:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010003.svg',
+    KC_FX_Defense_v1_0010004:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010004.svg',
+    KC_FX_Defense_v1_0010005:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010005.svg',
+    KC_FX_Defense_v1_0010006:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010006.svg',
+    KC_FX_Defense_v1_0010007:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010007.svg',
+    KC_FX_Defense_v1_0010008:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010008.svg',
+    KC_FX_Defense_v1_0010009:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010009.svg',
+    KC_FX_Defense_v1_0010010:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010010.svg',
+    KC_FX_Defense_v1_0010011:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010011.svg',
+    KC_FX_Defense_v1_0010012:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010012.svg',
+    KC_FX_Defense_v1_0010013:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010013.svg',
+    KC_FX_Defense_v1_0010014:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010014.svg',
+    KC_FX_Defense_v1_0010015:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010015.svg',
+    KC_FX_Defense_v1_0010016:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010016.svg',
+    KC_FX_Defense_v1_0010017:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010017.svg',
+    KC_FX_Defense_v1_0010018:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010018.svg',
+    KC_FX_Defense_v1_0010019:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010019.svg',
+    KC_FX_Defense_v1_0010020:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010020.svg',
+    KC_FX_Defense_v1_0010021:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010021.svg',
+    KC_FX_Defense_v1_0010022:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010022.svg',
+    KC_FX_Defense_v1_0010023:
+        'fx sequences/KC_FX_BlockDefense_v1_001/KC_FX_Defense_v1_0010023.svg',
+    KC_FX_Poison001_Player0001:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0001.svg',
+    KC_FX_Poison001_Player0002:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0002.svg',
+    KC_FX_Poison001_Player0003:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0003.svg',
+    KC_FX_Poison001_Player0004:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0004.svg',
+    KC_FX_Poison001_Player0005:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0005.svg',
+    KC_FX_Poison001_Player0006:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0006.svg',
+    KC_FX_Poison001_Player0007:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0007.svg',
+    KC_FX_Poison001_Player0008:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0008.svg',
+    KC_FX_Poison001_Player0009:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0009.svg',
+    KC_FX_Poison001_Player0010:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0010.svg',
+    KC_FX_Poison001_Player0011:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0011.svg',
+    KC_FX_Poison001_Player0012:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0012.svg',
+    KC_FX_Poison001_Player0013:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0013.svg',
+    KC_FX_Poison001_Player0014:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0014.svg',
+    KC_FX_Poison001_Player0015:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0015.svg',
+    KC_FX_Poison001_Player0016:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0016.svg',
+    KC_FX_Poison001_Player0017:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0017.svg',
+    KC_FX_Poison001_Player0018:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0018.svg',
+    KC_FX_Poison001_Player0019:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0019.svg',
+    KC_FX_Poison001_Player0020:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0020.svg',
+    KC_FX_Poison001_Player0021:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0021.svg',
+    KC_FX_Poison001_Player0022:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0022.svg',
+    KC_FX_Poison001_Player0023:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0023.svg',
+    KC_FX_Poison001_Player0024:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0024.svg',
+    KC_FX_Poison001_Player0025:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0025.svg',
+    KC_FX_Poison001_Player0026:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0026.svg',
+    KC_FX_Poison001_Player0027:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0027.svg',
+    KC_FX_Poison001_Player0028:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0028.svg',
+    KC_FX_Poison001_Player0029:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0029.svg',
+    KC_FX_Poison001_Player0030:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0030.svg',
+    KC_FX_Poison001_Player0031:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0031.svg',
+    KC_FX_Poison001_Player0032:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0032.svg',
+    KC_FX_Poison001_Player0033:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0033.svg',
+    KC_FX_Poison001_Player0034:
+        'fx sequences/KC_FX_Poison001_Player/KC_FX_Poison001_Player0034.svg',
+    // @endindex
+}
