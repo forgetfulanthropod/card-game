64038ce43fb3f018dd711057452e73ed265270a4
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2022-11-10 14:21:27 -0800

    adjusted shadows, colors, breakpoints, borders in start screen

--- client/components/NewStartScreen.tsx
+++ client/components/NewStartScreen.tsx
@@ -20,11 +20,11 @@ export function NewStartScreen(props: {
             className='max-w-full absolute -z-50'
         />
 
-        <div className='nav w-full row-span-1 flex p-2 xs:p-4 md:p-8 justify-between items-start'>
-            <div className='flex w-12 2xs:w-14 xs:w-20 sm:w-32 xl:w-56 cursor-pointer hover:scale-105 transition'>
+        <div className='nav w-full row-span-1 flex p-2 xs:p-4 lg:p-8 justify-between items-start'>
+            <div className='flex w-12 2xs:w-14 xs:w-20 sm:w-24 md:w-28 xl:w-56 cursor-pointer hover:scale-105 transition'>
                 <img src='./logos/KaijuCards.png' />
             </div>
-            <div className='navRight flex justify-between sm:pl-12 xs:pt-2 xs:pl-6 sm:pt-4 items-start w-full'>
+            <div className='navRight flex justify-between sm:pl-12 xs:pt-2 xs:pl-6 lg:pt-4 items-start w-full'>
                 <div className='grid grid-cols-4 mr-4'>
                     <NavIconWrapper>
                         <img
@@ -65,7 +65,7 @@ export function NewStartScreen(props: {
         </div>
 
         <div className='bottom h-full p-2 row-span-3 grid grid-cols-12'>
-            <div className='left-buttons h-full col-span-4 flex flex-col justify-end gap-2 lg:gap-4 xl:gap-6 px-3 p-1 sm:p-3 md:p-5 xl:p-10'>
+            <div className='left-buttons h-full col-span-4 flex flex-col justify-end gap-2 lg:gap-4 xl:gap-6 px-3 p-2  xl:p-10'>
                 <PrimaryButton
                     text='tutorial'
                     onClick={() =>
@@ -83,8 +83,8 @@ export function NewStartScreen(props: {
                     size='large'
                 />
             </div>
-            <div className='mid-buttons h-full col-span-4 flex items-end gap-2 sm:gap-8 p-1 sm:p-3 md:p-5 xl:p-10'>
-                <div className='h-1/6 w-full flex xl:pt-4 gap-2 xl:gap-4'>
+            <div className='mid-buttons h-full col-span-4 flex items-end gap-2 sm:gap-8 p-1 sm:p-2 xl:p-10'>
+                <div className='h-auto w-full flex xl:pt-4 gap-2 xl:gap-4'>
                     <PrimaryButton
                         text='quests'
                         onClick={() =>
@@ -106,7 +106,7 @@ export function NewStartScreen(props: {
                     />
                 </div>
             </div>
-            <div className='right-buttons h-full col-span-4 flex flex-col justify-end gap-2 sm:gap-4 xl:gap-6 p-1 px-3 sm:p-3 md:p-5 xl:p-10'>
+            <div className='right-buttons h-full col-span-4 flex flex-col justify-end gap-2 sm:gap-4 xl:gap-6 p-1 px-3 sm:p-2 md:px-4 xl:p-10'>
                 <GameModeContainer
                     text='Play The First Siege'
                     isNew
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./client/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0px 10px 20px 2px rgba(0, 0, 0, 0.4)',
+      },
+      dropShadow: {
+        '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.45)',
+          '0 45px 65px rgba(0, 0, 0, 0.25)'
+        ]
+      },
+    },
     screens: {
       '2xs': '365px',
       'xs': '480px',
