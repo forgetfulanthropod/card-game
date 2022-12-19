ca5cdb30f0d349917e35d1cf2418f744dda0a3d2
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2022-12-18 21:36:02 -0600

    wip in game menu

--- client/components/ResetButton.tsx
+++ client/components/ResetButton.tsx
@@ -1,9 +1,11 @@
 import { callApi } from '@/callApi'
 import { styled } from '@/config'
+import { useState, useRef, MouseEvent, useEffect } from 'react'
+import { useOutsideClickDismisser } from '@/hooks/useClickDismisser'
 
 const Root = styled.button`
     position: absolute;
-    right: 5px;
+    right: 20px;
     top: 10px;
     pointer-events: auto;
     border: none;
@@ -14,26 +16,78 @@ const Root = styled.button`
 `
 
 export function ResetButton(props: { username: string }): JSXElement {
+    const [showActions, setShowActions] = useState(false)
+    const actionsRef = useRef(null)
+    useOutsideClickDismisser(actionsRef, setShowActions)
+
+    const handleClick = () => {
+        setShowActions(actions => !actions)
+    }
+
+    const handleMuteSFX = () => {
+        //TODO
+    }
+
+    const handleMuteMusic = () => {
+        //TODO
+    }
+
     let release = false
-    return <Root
-        onPointerDown={() => {
-            release = false
-            setTimeout(async () => {
-                if (!release) {
+    return <div className='flex flex-col items-end text-white'>
+        <Root
+            onClick={() => handleClick()}
+            // onPointerDown={() => {
+            //     release = false
+            //     setTimeout(async () => {
+            //         if (!release) {
+            //             localStorage.removeItem('username')
+            //             await callApi('makeNewUser', {
+            //                 username: props.username,
+            //             })
+            //             window.location.reload()
+            //         }
+            //     }, 2500)
+            // }}
+            // onPointerUp={async () => {
+            //     release = true
+            //     // window.location.reload()
+            //     // location.replace(
+            //     //     window.location.href.replace('#continue', '') + '#continue'
+            //     // )
+            // }}
+        >
+            ⚙
+        </Root>
+        <div
+            className={`pointer-events-auto absolute top-14 right-6 flex flex-col text-white mt-2 rounded-xl bg-stone-700 font-sans p-1 w-auto font-medium z-50 text-sm shadow-3xl transition-all ${
+                showActions ? 'opacity-100 visible' : 'opacity-0 invisible'
+            }`}
+            ref={actionsRef}
+        >
+            <button
+                className='px-8 py-2 hover:bg-stone-900 rounded-lg z-50'
+                onClick={handleMuteSFX}
+            >
+                Mute SFX
+            </button>
+            <button
+                className='px-8 py-2 hover:bg-stone-900 rounded-lg z-50'
+                onClick={handleMuteMusic}
+            >
+                Mute Music
+            </button>
+            <button
+                className='px-8 py-2 hover:bg-stone-900 rounded-lg z-50'
+                onClick={async () => {
                     localStorage.removeItem('username')
-                    await callApi('makeNewUser', { username: props.username })
+                    await callApi('makeNewUser', {
+                        username: props.username,
+                    })
                     window.location.reload()
-                }
-            }, 2500)
-        }}
-        onPointerUp={async () => {
-            release = true
-            window.location.reload()
-            // location.replace(
-            //     window.location.href.replace('#continue', '') + '#continue'
-            // )
-        }}
-    >
-        ↺
-    </Root>
+                }}
+            >
+                Abandon Run
+            </button>
+        </div>
+    </div>
 }
