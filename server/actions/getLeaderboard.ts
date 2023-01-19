e2e2b8aa8f055255da99956474bc42c9ebd4b5ab
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-01-19 20:27:15 +0900

    fix ts error

--- server/actions/getLeaderboard.ts
+++ server/actions/getLeaderboard.ts
@@ -6,12 +6,12 @@ import {
 } from 'shared'
 import { getDbClient, sql as sqlTag } from '../db/client'
 
-export const getLeaderboard: ServerActions['getLeaderboard'] = async () => {
+export const getLeaderboard: ServerActions['getLeaderboard'] = async (args) => {
     logger.info(`Getting leaderboards...`)
     const connection = await getDbClient()
     let sql = sqlTag.typeAlias('leaderboard')
     const leaderboard: Leaderboard = await connection.many(sql`
         SELECT * FROM user_run_leaderboard order by highest_score desc
     `)
-    return { leaderboard }
+    return leaderboard
 }
