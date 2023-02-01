ac5e73a95d1008fc17e00dea21d3bc5cf2600331
Charlie C Miles <charliecmiles@gmail.com>
2023-02-01 13:34:07 -0800

    change password for demo branch

--- scripts/clientEsbuild.mjs
+++ scripts/clientEsbuild.mjs
@@ -12,7 +12,7 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import { config as loadDotEnv } from 'dotenv'
 
-const password = 'dailyship'
+const password = 'mullover'
 const buildDir = 'public/'
 const entryPoint = 'client/index.tsx'
 const outFile = `${buildDir}/${password}.js`
