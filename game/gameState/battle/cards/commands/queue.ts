48db8b58b63be2428e0f2e444559145ee9973469
Charlie C Miles <charliecmiles@gmail.com>
2023-02-09 13:09:02 -0800

    patient ambush text fix.. it used to be right?

--- game/gameState/battle/cards/commands/queue.ts
+++ game/gameState/battle/cards/commands/queue.ts
@@ -1,12 +1,11 @@
 import { evalAll, Executors, Explainers } from './util'
-import { evalAllAsHtml, s } from './util'
 import { enqueueAction } from '@/gameState'
 
 export const explain: Explainers['queue'] = dslArgs => {
     const [turnsAway, nextAction] = evalAll(dslArgs)
 
     return `${
-        turnsAway === 1 ? 'next turn' : `in ${turnsAway} turns`
+        turnsAway === 1 ? 'Next turn' : `In ${turnsAway} turns`
     }, ${nextAction}`
 }
 
