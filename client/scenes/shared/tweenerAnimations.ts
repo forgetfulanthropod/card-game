931525bd5e2dd08b60a14fb34e137d8e94cf5622
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-03-10 02:05:44 -0600

    added utility to animate Tweener by keyframes

--- client/scenes/shared/tweenerAnimations.ts
+++ client/scenes/shared/tweenerAnimations.ts
@@ -1,10 +1,19 @@
-import { TweenablePixiContainer } from '@/elementsUtil'
+import { ContainerArgs, TweenablePixiContainer } from '@/elementsUtil'
+import { max } from 'lodash'
 import { Easing, Tweener } from 'pixi-tweener'
 
 // TODO: add other tweener animation fns here
 
+type DurationInSeconds = number
+type DurationInKeyframes = number
+type TweenerKeyframe = Omit<ContainerArgs, 'scale'> & {
+    keyframes: DurationInKeyframes
+    ease?: (pos: number) => number
+    tweenableScale?: number
+}
+
 export const animateBounceScale = async (el: TweenablePixiContainer) => {
-    const { x, y, tweenableScale } = el
+    const { tweenableScale } = el
 
     await Tweener.add(
         {
@@ -27,3 +36,103 @@ export const animateBounceScale = async (el: TweenablePixiContainer) => {
         }
     )
 }
+
+/**
+ *
+ * @param target Element to be animated.
+ * @param animationDuration Total animation sequence duration in seconds - to calculate how long each keyframe should last.
+ * @param tweenerKeyframe All TweenerKeyframes will be executed synchronously; Arrays of TweenerKeyframes will execute all of their individual keyframes in parallel and are treated like one single keyframe of length = max(keyframes count in array). In other words, the promise parsing the array is resolved when its longest animation is complete.
+ * @returns A promise that resolves when all animations complete.
+ */
+export const runKeyframeAnimations = async (
+    target: TweenablePixiContainer,
+    animationDuration: DurationInSeconds,
+    ...tweenerKeyframes: Array<TweenerKeyframe | TweenerKeyframe[]>
+) => {
+    const keyframeCount = getKeyframeCount(...tweenerKeyframes)
+    const durationPerKeyframe = animationDuration / keyframeCount
+    for (let keyframe of tweenerKeyframes) {
+        if (Array.isArray(keyframe)) {
+            await runParallelAnimations(target, durationPerKeyframe, keyframe)
+        } else {
+            await runAnimation(target, durationPerKeyframe, keyframe)
+        }
+    }
+    return void 0
+}
+
+const getKeyframeCount = (
+    ...tweenerKeyframes: Array<TweenerKeyframe | TweenerKeyframe[]>
+): DurationInKeyframes => {
+    const sumReducer = (prev: number, curr: number) => prev + curr
+    return tweenerKeyframes
+        .map(keyframe => {
+            if (Array.isArray(keyframe)) {
+                return max(keyframe.map(kf => kf.keyframes)) ?? 0
+            } else {
+                return keyframe.keyframes
+            }
+        })
+        .reduce(sumReducer)
+}
+
+const runParallelAnimations = async (
+    target: TweenablePixiContainer,
+    durationPerKeyframe: DurationInSeconds,
+    tweenerKeyframes: TweenerKeyframe[]
+) => {
+    const keyframeCount = getKeyframeCount(tweenerKeyframes)
+    return await new Promise(resolve => {
+        tweenerKeyframes.forEach(
+            async kf => await runAnimation(target, durationPerKeyframe, kf)
+        )
+        setTimeout(() => {
+            resolve(void 0)
+        }, keyframeCount * durationPerKeyframe * 1000)
+    })
+}
+
+const runAnimation = async (
+    target: TweenablePixiContainer,
+    durationPerKeyframe: DurationInSeconds,
+    tweenerKeyframe: TweenerKeyframe
+) => {
+    const { keyframes, ease, ...props } = tweenerKeyframe
+    const duration = keyframes * durationPerKeyframe
+    return Tweener.add(
+        {
+            //@ts-ignore
+            target,
+            duration,
+            ease,
+        },
+        { ...props }
+    )
+}
+
+/**
+ * @tutorial This animation lasts 2 seconds and has a total of 30 keyframes, so each keyframe lasts roughly 0.067 seconds
+ */
+const keyframeExample = () =>
+    runKeyframeAnimations(
+        new TweenablePixiContainer(),
+        2,
+        {
+            keyframes: 10,
+            x: 5,
+        },
+        {
+            keyframes: 5,
+            x: 10,
+        },
+        [
+            { keyframes: 10, tweenableScale: 2 },
+            { keyframes: 25, y: 10 },
+            { keyframes: 10, x: 25 },
+        ],
+        {
+            keyframes: 5,
+            x: 3,
+            y: 4,
+        }
+    )
