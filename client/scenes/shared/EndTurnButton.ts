421462ee5facb1d16e820e361ba5a4c19c30eed5
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-03-29 13:41:06 +0200

    add new end turn button, button component

--- client/scenes/shared/EndTurnButton.ts
+++ client/scenes/shared/EndTurnButton.ts
@@ -11,9 +11,15 @@ import { callApi } from '@/callApi'
 import { hoveredCharacterUid, toDatum } from '@/util'
 import { getBattleScene } from '@/data'
 import { compose } from 'datums'
+import { GradientButton } from './GradientButton'
 
 export function EndTurnButton(): PixiContainer {
-    const buttonTexture = getTexture('endTurnButton')
+    const onClick = async () => {
+        hoveredCharacterUid.set(null)
+        await callApi('endTurn', {})
+    }
+    const text = 'End Turn'
+
     return If(
         compose(
             ([isPlayerTurn, isInBattle]) => isPlayerTurn && isInBattle,
@@ -24,30 +30,15 @@ export function EndTurnButton(): PixiContainer {
         ),
         // toDatum(getBattleScene().select('isPlayerTurn'), () => true), // TEMP!! IMPROPER isPlayerTurn state BUG!!!!
         () =>
-            Container(
-                {
-                    x: BASE_WIDTH * 0.9,
-                    y: BASE_HEIGHT * 0.78,
-                    onClick: async () => {
-                        hoveredCharacterUid.set(null)
-                        await callApi('endTurn', {})
-                    },
-                },
-
-                Sprite({
-                    src: buttonTexture,
-                    anchor: [0.5, 0.5],
-                    scale: (BASE_WIDTH * 0.15) / buttonTexture.width,
-                })
-                // Text({
-                //     text: 'End Turn',
-                //     anchor: [0.5, 0.5],
-                //     style: {
-                //         fill: 0xffffff,
-                //         fontSize: 44,
-                //         fontFamily: 'bigFont',
-                //     },
-                // }),
-            )
+            GradientButton({
+                onClick,
+                text,
+                fontSize: 40,
+                gradientFrom: 0xFF9534,
+                gradientTo: 0xEC4F0C,
+                borderColor: 0x330004,
+                x: BASE_WIDTH * 0.90,
+                y: BASE_HEIGHT * 0.78,
+            })
     )
 }
---
+++ client/scenes/shared/GradientButton.ts
@@ -0,0 +1,115 @@
+import {
+    addFilterTo,
+    customGlowFilter,
+    fontMap,
+    onDestroyed,
+    removeFilterFrom,
+} from '@/elementsUtil'
+import {
+    Container,
+    Text,
+    RoundedRectangleGradientSprite,
+} from '@/elementsUtil'
+import {
+    OutlineFilter,
+    HslAdjustmentFilter,
+} from 'pixi-filters'
+import { Easing, Tweener } from 'pixi-tweener'
+
+export function GradientButton(args: {
+    onClick: Callback
+    text: string
+    x: number
+    y: number
+    fontSize?: number
+    gradientFrom?: number
+    gradientTo?: number
+    borderColor?: number
+    padding?: number
+}) {
+    const {
+        text,
+        fontSize = 30,
+        gradientFrom,
+        gradientTo,
+        x,
+        y,
+        onClick,
+        borderColor,
+        padding,
+    } = args
+
+    const ButtonText = Text({
+        text: text.toUpperCase(),
+        style: {
+            fill: 'white',
+            fontFamily: fontMap['bigFont'],
+            fontSize: fontSize,
+        },
+        anchor: [0.5, 0.5],
+    })
+
+    const ButtonBg = RoundedRectangleGradientSprite({
+        spriteArgs: {
+            // width: padding ? ButtonText.width + padding * 10 : ButtonText.width,
+            width: ButtonText.width + 100,
+            height: ButtonText.height + 25,
+            x: 0,
+            y: 0,
+            name: 'ButtonGradientBg',
+            anchor: [0.5, 0.5],
+            alpha: 1,
+        },
+        radius: 12,
+        gradientArgs: {
+            x0: 0,
+            x1: 250,
+            y0: 0,
+            y1: 0,
+            colorStops: [
+                { color: gradientFrom ?? 0x109f10, offset: 0 },
+                { color: gradientTo ?? 0x36e736, offset: 1 },
+            ],
+        },
+    })
+
+    const outlineFilter = new OutlineFilter(5, borderColor ?? 0x002717)
+    const hoverFilter = new HslAdjustmentFilter({ lightness: -0.2 })
+
+    outlineFilter.resolution = 2
+
+    ButtonBg.filters = [outlineFilter]
+    ButtonText.filters = [outlineFilter]
+
+    ButtonBg.cursor = `url('assets/root/hand.webp'), pointer`
+
+    const root = Container(
+        {
+            x,
+            y,
+            onClick,
+            events: {
+                pointerenter() {
+                    //@ts-ignore
+                    hoverFilter.alpha = 0
+                    addFilterTo(ButtonBg, hoverFilter)
+                    Tweener.add(
+                        //@ts-ignore
+                        { target: hoverFilter, duration: 0.1, ease: Easing.easeTo },
+                        { alpha: 1 }
+                    )
+                },
+                pointerleave() {
+                    removeFilterFrom(ButtonBg, hoverFilter)
+                },
+            },
+        },
+        ButtonBg,
+        ButtonText
+    )
+
+    return onDestroyed(root, () => {
+        outlineFilter.destroy()
+        hoverFilter.destroy()
+    })
+}
